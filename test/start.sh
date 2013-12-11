47a92ff273ced167388fe8f7b2bcb0b6da8333eb
Alex Kocharin <alex@kocharin.ru>
2013-12-12 00:51:48 +0400

    fail-safe semver parsing + tests, ref #25

--- test/start.sh
+++ test/start.sh
@@ -4,6 +4,6 @@ CWD=$(pwd)
 PATH='../node_modules/.bin':$PATH
 TESTDIR=$(dirname $0)
 cd $TESTDIR
-mocha -R list --ui exports ./tests.js ./no_proxy.js
+mocha -R list --ui exports ./tests.js ./no_proxy.js ./st_merge.js
 cd $CWD
 
