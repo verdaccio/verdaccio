ee16b06b3a6f6900ab80172db109b5d482f583c8
Alex Kocharin <alex@kocharin.ru>
2013-12-13 18:00:46 +0400

    style fix

--- test/config.js
+++ test/config.js
@@ -1,13 +1,13 @@
-var assert = require('assert');
-var ex = module.exports;
+var assert = require('assert')
+  , ex = module.exports
 
 ex['trying to fetch non-existent package'] = function(cb) {
-	var f = fork('../bin/sinopia', ['-c', './config/log-1.yaml'], {silent: true});
+	var f = fork('../bin/sinopia', ['-c', './config/log-1.yaml'], {silent: true})
 	f.on('message', function(msg) {
 		if ('sinopia_started' in msg) {
-			f.kill();
-			cb();
+			f.kill()
+			cb()
 		}
-	});
-};
+	})
+}
 
--- test/mirror.js
+++ test/mirror.js
@@ -1,75 +1,75 @@
-var assert = require('assert');
-var readfile = require('fs').readFileSync;
-var ex = module.exports;
-var server = process.server;
-var server2 = process.server2;
+var assert = require('assert')
+  , readfile = require('fs').readFileSync
+  , ex = module.exports
+  , server = process.server
+  , server2 = process.server2
 
 ex['testing anti-loop'] = function(cb) {
 	server2.get_package('testloop', function(res, body) {
-		assert.equal(res.statusCode, 404);
-		assert(~body.error.indexOf('no such package'));
-		cb();
-	});
-};
+		assert.equal(res.statusCode, 404)
+		assert(~body.error.indexOf('no such package'))
+		cb()
+	})
+}
 
 ['fwd', /*'loop'*/].forEach(function(pkg) {
-	var prefix = pkg+': ';
-	pkg = 'test'+pkg;
+	var prefix = pkg + ': '
+	pkg = 'test' + pkg
 
 	ex[prefix+'creating new package'] = function(cb) {
 		server.put_package(pkg, require('./lib/package')(pkg), function(res, body) {
-			assert.equal(res.statusCode, 201);
-			assert(~body.ok.indexOf('created new package'));
-			cb();
-		});
-	};
+			assert.equal(res.statusCode, 201)
+			assert(~body.ok.indexOf('created new package'))
+			cb()
+		})
+	}
 
 	ex[prefix+'uploading new package version'] = function(cb) {
 		server.put_version(pkg, '0.1.1', require('./lib/package')(pkg), function(res, body) {
-			assert.equal(res.statusCode, 201);
-			assert(~body.ok.indexOf('published'));
-			cb();
-		});
-	};
+			assert.equal(res.statusCode, 201)
+			assert(~body.ok.indexOf('published'))
+			cb()
+		})
+	}
 
 	ex[prefix+'downloading package via server2'] = function(cb) {
 		server2.get_package(pkg, function(res, body) {
-			assert.equal(res.statusCode, 200);
-			assert.equal(body.name, pkg);
-			assert.equal(body.versions['0.1.1'].name, pkg);
-			assert.equal(body.versions['0.1.1'].dist.tarball, 'http://localhost:55552/'+pkg+'/-/blahblah');
-			cb();
-		});
-	};
+			assert.equal(res.statusCode, 200)
+			assert.equal(body.name, pkg)
+			assert.equal(body.versions['0.1.1'].name, pkg)
+			assert.equal(body.versions['0.1.1'].dist.tarball, 'http://localhost:55552/'+pkg+'/-/blahblah')
+			cb()
+		})
+	}
 
 	ex[prefix+'uploading incomplete tarball'] = function(cb) {
 		server.put_tarball_incomplete(pkg, pkg+'.bad', readfile('fixtures/binary'), 3000, function(res, body) {
-			cb();
-		});
-	};
+			cb()
+		})
+	}
 
 	ex[prefix+'uploading new tarball'] = function(cb) {
 		server.put_tarball(pkg, pkg+'.file', readfile('fixtures/binary'), function(res, body) {
-			assert.equal(res.statusCode, 201);
-			assert(body.ok);
-			cb();
-		});
-	};
+			assert.equal(res.statusCode, 201)
+			assert(body.ok)
+			cb()
+		})
+	}
 
 	ex[prefix+'downloading tarball from server1'] = function(cb) {
 		server.get_tarball(pkg, pkg+'.file', function(res, body) {
-			assert.equal(res.statusCode, 200);
-			assert.deepEqual(body, readfile('fixtures/binary').toString('utf8'));
-			cb();
-		});
-	};
+			assert.equal(res.statusCode, 200)
+			assert.deepEqual(body, readfile('fixtures/binary').toString('utf8'))
+			cb()
+		})
+	}
 
 	ex[prefix+'downloading tarball from server2'] = function(cb) {
 		server2.get_tarball(pkg, pkg+'.file', function(res, body) {
-			assert.equal(res.statusCode, 200);
-			assert.deepEqual(body, readfile('fixtures/binary').toString('utf8'));
-			cb();
-		});
-	};
-});
+			assert.equal(res.statusCode, 200)
+			assert.deepEqual(body, readfile('fixtures/binary').toString('utf8'))
+			cb()
+		})
+	}
+})
 
