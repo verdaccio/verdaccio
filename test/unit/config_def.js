8f0039de928c4b3285d1849a6ce5bb205600d9cb
Alex Kocharin <alex@kocharin.ru>
2014-09-14 19:49:15 +0400

    '@all' -> '$all' (avoid yaml identifiers)

---
+++ test/unit/config_def.js
@@ -0,0 +1,8 @@
+
+describe('config.yaml', function() {
+	it('should be parseable', function() {
+		var source = require('fs').readFileSync(__dirname + '/../../lib/config_def.yaml', 'utf8')
+		require('js-yaml').safeLoad(source)
+	})
+})
+
