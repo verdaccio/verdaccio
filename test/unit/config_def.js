c78390752a3178226d53f5be0823fcfbf1ab9fad
Alex Kocharin <alex@kocharin.ru>
2014-11-12 19:45:40 +0300

    fix tests

--- test/unit/config_def.js
+++ test/unit/config_def.js
@@ -1,7 +1,7 @@
 
 describe('config.yaml', function() {
   it('should be parseable', function() {
-    var source = require('fs').readFileSync(__dirname + '/../../lib/config_def.yaml', 'utf8')
+    var source = require('fs').readFileSync(__dirname + '/../../conf/default.yaml', 'utf8')
     require('js-yaml').safeLoad(source)
   })
 })
