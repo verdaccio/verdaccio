4c11a42d213b616b95fef511014a5ed30392a1fa
Alex Kocharin <alex@kocharin.ru>
2014-11-13 20:13:37 +0300

    separate web and api routers to different files

--- test/unit/validate_all.js
+++ test/unit/validate_all.js
@@ -2,32 +2,38 @@
 
 var assert = require('assert')
 
-describe('index.js app', function() {
-  var source = require('fs').readFileSync(__dirname + '/../../lib/index.js', 'utf8')
+describe('index.js app', test('index.js'))
+describe('index-api.js app', test('index-api.js'))
+describe('index-web.js app', test('index-web.js'))
 
-  var very_scary_regexp = /\n\s*app\.(\w+)\s*\(\s*(("[^"]*")|('[^']*'))\s*,/g
-  var m
-  var params = {}
+function test(file) {
+  return function() {
+    var source = require('fs').readFileSync(__dirname + '/../../lib/' + file, 'utf8')
 
-  while ((m = very_scary_regexp.exec(source)) != null) {
-    if (m[1] === 'set') continue
+    var very_scary_regexp = /\n\s*app\.(\w+)\s*\(\s*(("[^"]*")|('[^']*'))\s*,/g
+    var m
+    var params = {}
 
-    var inner = m[2].slice(1, m[2].length-1)
-    var t
+    while ((m = very_scary_regexp.exec(source)) != null) {
+      if (m[1] === 'set') continue
 
-    inner.split('/').forEach(function(x) {
-      if (m[1] === 'param') {
-        params[x] = 'ok'
-      } else if (t = x.match(/^:([^?:]*)\??$/)) {
-        params[t[1]] = params[t[1]] || m[0].trim()
-      }
+      var inner = m[2].slice(1, m[2].length-1)
+      var t
+
+      inner.split('/').forEach(function(x) {
+        if (m[1] === 'param') {
+          params[x] = 'ok'
+        } else if (t = x.match(/^:([^?:]*)\??$/)) {
+          params[t[1]] = params[t[1]] || m[0].trim()
+        }
+      })
+    }
+
+    Object.keys(params).forEach(function(param) {
+      it('should validate ":'+param+'"', function() {
+        assert.equal(params[param], 'ok')
+      })
     })
   }
-
-  Object.keys(params).forEach(function(param) {
-    it('should validate ":'+param+'"', function() {
-      assert.equal(params[param], 'ok')
-    })
-  })
-})
+}
 
