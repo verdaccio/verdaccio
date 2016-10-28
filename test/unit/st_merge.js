ee5d8d955a2e85b28d49efa51d4e8a13d76afe06
Ryan Graham <r.m.graham@gmail.com>
2016-10-28 12:25:44 -0700

    don't blindly clobber local dist-tags
    If packages are being published to verdaccio as well as upstream to
    npmjs.org, then when the cache is updated from npmjs.org it uses the
    dist-tags from the upstream even if the locally published version is
    actually newer. This makes it very difficult to use verdaccio as a
    staging registry for testing out potential releases.
    
    This change partially reverts a change in behaviour that was introduced
    in #8 which caused a regression for the staging style workflow that was
    supported by sinopia.

--- test/unit/st_merge.js
+++ test/unit/st_merge.js
@@ -29,6 +29,23 @@ describe('Merge', function() {
     })
   })
 
+  it('dist-tags - staging', function() {
+    var x = {
+      versions: {},
+      // we've been locally publishing 1.1.x in preparation for the next
+      // public release
+      'dist-tags': {q:'1.1.10',w:'2.2.2'},
+    }
+    // 1.1.2 is the latest public release, but we want to continue testing
+    // against our local 1.1.10, which may end up published as 1.1.3 in the
+    // future
+    merge(x, {'dist-tags':{q:'1.1.2',w:'3.3.3',t:'4.4.4'}})
+    assert.deepEqual(x, {
+      versions: {},
+      'dist-tags': {q:'1.1.10',w:'3.3.3',t:'4.4.4'},
+    })
+  })
+
   it('semver_sort', function() {
     assert.deepEqual(semver_sort(['1.2.3','1.2','1.2.3a','1.2.3c','1.2.3-b']),
     [ '1.2.3a',
