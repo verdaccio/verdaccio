5b1f029a07af51c87c9465853b25cbcc088919bf
Meeeeow <me@async.sh>
2017-07-10 00:11:36 +0800

    fix: :bug: use hash router to avoid pathname conflict with package name

--- src/webui/src/App.jsx
+++ src/webui/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css'
 
