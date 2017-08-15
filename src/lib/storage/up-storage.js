eb0c87d77a6a56feb92058056ed3098210558f2a
J. Russell Smyth <jrussell.smyth@gmail.com>
2017-08-15 23:59:00 +0000

    add semicolon after media type in accept statement. Necessary for more pedantic servers such as Artifactory

--- src/lib/storage/up-storage.js
+++ src/lib/storage/up-storage.js
@@ -17,7 +17,7 @@ const encode = function(thing) {
 
 const jsonContentType = 'application/json';
 
-const contenTypeAccept = `${jsonContentType} q=0.8, */*`;
+const contenTypeAccept = `${jsonContentType}; q=0.8, */*`;
 
 /**
  * Just a helper (`config[key] || default` doesn't work because of zeroes)
