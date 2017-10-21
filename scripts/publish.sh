4995d19d3ee590a1225586d98c234ac45ff065cf
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2017-10-21 10:38:29 +0200

    build: remove incremental versionin, this is done by npm run release script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,8 +6,5 @@ lastTag=$(git describe --tags $(git rev-list --tags --max-count=1))
 # Print it to the console for verification
 echo "Bumping version to new tag: ${lastTag}"
 
-# Bump the version
-npm --no-git-tag-version version $lastTag
-
 # Publish to NPM
 npm publish
