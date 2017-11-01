9e72db1e7e58f2a348350aa02d984a66e9597814
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2017-11-01 17:47:20 +0100

    refactor: migrate unit test to Jest

--- test/unit/no_proxy.spec.js
+++ test/unit/no_proxy.spec.js
@@ -10,18 +10,18 @@ function setupProxy(host, config, mainconfig) {
   return new Storage(config, mainconfig);
 }
 
-describe('Use proxy', function() {
-  it('should work fine without proxy', function() {
+describe('Use proxy', () => {
+  test('should work fine without proxy', () => {
     let x = setupProxy('http://x/x', {}, {});
     assert.equal(x.proxy, null);
   });
 
-  it('local config should take priority', function() {
+  test('local config should take priority', () => {
     let x = setupProxy('http://x/x', {http_proxy: '123'}, {http_proxy: '456'});
     assert.equal(x.proxy, '123');
   });
 
-  it('no_proxy is invalid', function() {
+  test('no_proxy is invalid', () => {
     let x = setupProxy('http://x/x', {http_proxy: '123', no_proxy: false}, {});
     assert.equal(x.proxy, '123');
     x = setupProxy('http://x/x', {http_proxy: '123', no_proxy: null}, {});
@@ -32,17 +32,17 @@ describe('Use proxy', function() {
     assert.equal(x.proxy, '123');
   });
 
-  it('no_proxy - simple/include', function() {
+  test('no_proxy - simple/include', () => {
     let x = setupProxy('http://localhost', {http_proxy: '123'}, {no_proxy: 'localhost'});
     assert.equal(x.proxy, undefined);
   });
 
-  it('no_proxy - simple/not', function() {
+  test('no_proxy - simple/not', () => {
     let x = setupProxy('http://localhost', {http_proxy: '123'}, {no_proxy: 'blah'});
     assert.equal(x.proxy, '123');
   });
 
-  it('no_proxy - various, single string', function() {
+  test('no_proxy - various, single string', () => {
     let x = setupProxy('http://blahblah', {http_proxy: '123'}, {no_proxy: 'blah'});
     assert.equal(x.proxy, '123');
     x = setupProxy('http://blah.blah', {}, {http_proxy: '123', no_proxy: 'blah'});
@@ -57,7 +57,7 @@ describe('Use proxy', function() {
     assert.equal(x.proxy, '123');
   });
 
-  it('no_proxy - various, array', function() {
+  test('no_proxy - various, array', () => {
     let x = setupProxy('http://blahblah', {http_proxy: '123'}, {no_proxy: 'foo,bar,blah'});
     assert.equal(x.proxy, '123');
     x = setupProxy('http://blah.blah', {http_proxy: '123'}, {no_proxy: 'foo,bar,blah'});
@@ -72,14 +72,14 @@ describe('Use proxy', function() {
     assert.equal(x.proxy, null);
   });
 
-  it('no_proxy - hostport', function() {
+  test('no_proxy - hostport', () => {
     let x = setupProxy('http://localhost:80', {http_proxy: '123'}, {no_proxy: 'localhost'});
     assert.equal(x.proxy, null);
     x = setupProxy('http://localhost:8080', {http_proxy: '123'}, {no_proxy: 'localhost'});
     assert.equal(x.proxy, null);
   });
 
-  it('no_proxy - secure', function() {
+  test('no_proxy - secure', () => {
     let x = setupProxy('https://something', {http_proxy: '123'}, {});
     assert.equal(x.proxy, null);
     x = setupProxy('https://something', {https_proxy: '123'}, {});
