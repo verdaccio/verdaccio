6ee083d1e1b31701aeb9f7501aa5940f25e545c6
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2017-11-17 08:44:37 +0100

    refactor: Update cli path and fix eslint errors for transpiled code

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,3 +1,5 @@
+/* eslint prefer-rest-params:off */
+
 module.exports.spliceURL = function spliceURL() {
   return Array.from(arguments).reduce((lastResult, current) => lastResult + current).replace(/([^:])(\/)+(.)/g, `$1/$3`);
 };
