f6ea28274207973964e939d25db964e676ad3788
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-12-03 15:44:17 +0530

    webui: tests and dependency mock fixes
    deps fixes
    
    deps fixes

--- jest.config.js
+++ jest.config.js
@@ -22,9 +22,7 @@ module.exports = {
   'moduleNameMapper': {
     '\\.(scss)$': '<rootDir>/node_modules/identity-obj-proxy',
     'github-markdown-css': '<rootDir>/node_modules/identity-obj-proxy',
-    '\\.(png)$': '<rootDir>/node_modules/identity-obj-proxy',
-    'api': '<rootDir>/test/webui/components/__mocks__/api.js',
-    'storage': '<rootDir>/test/webui/components/__mocks__/storage.js',
+    '\\.(png)$': '<rootDir>/node_modules/identity-obj-proxy'
   },
   'transformIgnorePatterns': [
     '<rootDir>/node_modules/(?!react-syntax-highlighter)'
