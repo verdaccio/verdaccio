9bcbe79560371a932531b36230c9d6dc427f4fb5
Meeeeow <me@async.sh>
2017-12-03 23:22:45 +0800

    refactor: :hammer: move string md5 and get avatar to utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,5 +1,13 @@
-/* eslint prefer-rest-params:off */
+// @flow
+import crypto from 'crypto';
 
-export const spliceURL = function spliceURL(...args) {
+export function spliceURL(...args: Array<string>): string {
   return Array.from(args).reduce((lastResult, current) => lastResult + current).replace(/([^:])(\/)+(.)/g, `$1/$3`);
-};
+}
+
+/**
+ * Get MD5 from string
+ */
+export function stringToMD5(string: string): string {
+  return crypto.createHash('md5').update(string).digest('hex');
+}
