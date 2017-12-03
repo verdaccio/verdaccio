36fd639a907273d80eb3ce772425828960137ce2
Meeeeow <me@async.sh>
2017-12-04 00:34:34 +0800

    refactor: :hammer: follow review, split modules to multiple components

---
+++ src/webui/src/components/PackageSidebar/modules/Maintainers/MaintainerInfo/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './style.scss';
+
+export default function MaintainerInfo({title, name, avatar}) {
+  let avatarDescription = `${title} ${name}'s avatar`;
+  return (
+    <div className={classes.maintainer} title={name}>
+      <img src={avatar} alt={avatarDescription} title={avatarDescription}/>
+      <span>{name}</span>
+    </div>
+  );
+}
+MaintainerInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired
+};
--- src/webui/utils/getRegistryURL.js
+++
@@ -1,4 +0,0 @@
-export default function getRegistryURL() {
-  // Don't add slash if it's not a sub directory
-  return `${location.origin}${location.pathname === '/' ? '' : location.pathname}`;
-}
