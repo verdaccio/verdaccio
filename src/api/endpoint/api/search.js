69418fbe4a96dce414c123a480dbd91105c3ed02
Pascal <pascal@stim.fr>
2017-12-05 10:34:31 +0100

    new search command compatible with npm search

--- src/api/endpoint/api/search.js
+++ src/api/endpoint/api/search.js
@@ -96,4 +96,103 @@ module.exports = function(route, auth, storage) {
       check_finish();
     });
   });
+  
+  //searching packages
+  // GET /-/v1/search?text=string&size=20
+  route.get('/-/v1(\/search)?', function(req, res) {
+    let received_end = false;
+    let response_finished = false;
+    let processing_pkgs = 0;
+    let firstPackage = true;
+    let total = 0;
+
+    res.status(200);
+
+    /*
+     * Offical NPM registry (registry.npmjs.org) no longer return whole database,
+     * They only return packages matched with keyword in `referer: search pkg-name`,
+     * And NPM client will request server in every search.
+     *
+     * when request /-/v1/search, response is an json {"objects":[{"package": {}}],"total":20,"time":"Mon Dec 04 2017 13:46:30 GMT+0100 (CET)"}
+     */
+    res.set('Date', 'Mon, 10 Oct 1983 00:12:48 GMT');
+    const check_finish = function() {
+      if (!received_end) {
+        return;
+      }
+      if (processing_pkgs) {
+        return;
+      }
+      if (response_finished) {
+        return;
+      }
+      response_finished = true;
+      res.end('],"total":'+total+',"time":"'+new Date().toString()+'"}');
+    };
+    
+    res.write('{"objects":[');
+    // startkey >= mtime of module
+    let stream = storage.search(req.query.startkey || 0, {req: req});
+    let size = req.query.size || -1;
+
+    stream.on('data', function each(pkg) {
+      processing_pkgs++;
+
+      auth.allow_access(pkg.name, req.remote_user, function(err, allowed) {
+        processing_pkgs--;
+
+        if (err) {
+          if (err.status && String(err.status).match(/^4\d\d$/)) {
+            // auth plugin returns 4xx user error,
+            // that's equivalent of !allowed basically
+            allowed = false;
+          } else {
+            stream.abort(err);
+          }
+        }
+
+        if (allowed) {
+          if ((req.query.text==undefined || pkg.name.indexOf(req.query.text)!==-1) &&
+              (size == -1 || size>=1))
+          {
+			if (size>0) size--;
+			res.write(`${firstPackage ? '{"package":' : ',{"package":'}${JSON.stringify(standardizePkg(pkg))}}`);
+			//res.write(`${firstPackage ? '{"package":' : ',{"package":'}${JSON.stringify(pkg)},"flags":{"unstable":true},"score":{"final":0.144979397377621,"detail":{"quality":0.30431600667044645,"popularity":0.0033870077459281825,"maintenance":0.1499975504726063}},"searchScore":1.5267507e-13}`);
+			total++;
+			if (firstPackage) {
+			  firstPackage = false;
+			}
+          }
+        }
+
+        check_finish();
+      });
+    });
+
+    stream.on('error', function(_err) {
+      res.socket.destroy();
+    });
+
+    stream.on('end', function() {
+      received_end = true;
+      check_finish();
+    });
+  });
 };
+
+function standardizePkg (data) {
+  return {
+    name: data.name,
+    description: data.description,
+    maintainers: (data.maintainers || []).map(function (m) {
+      return { username: m.name, email: m.email }
+    }),
+    keywords: data.keywords || [],
+    version: Object.keys(data.versions || {})[0] || [],
+    date: (
+      data.time &&
+      data.time.modified &&
+      new Date(data.time.modified)
+    ) || null
+  }
+}
