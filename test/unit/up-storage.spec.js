70dbbb7ccb9c1e107723b5d624e8e87dd4005337
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2017-12-19 09:56:40 +0100

    test: add unit test for up-storage, getRemoteMetadata, fetchTarball

--- test/unit/up-storage.spec.js
+++ test/unit/up-storage.spec.js
@@ -25,14 +25,56 @@ describe('UpStorge', () => {
     expect(proxy).toBeDefined();
   });
 
-  test('should be get remote metadata', (done) => {
-    const proxy = generateProxy();
-    proxy.getRemoteMetadata('jquery', {}, (err, data, etag) => {
-      expect(err).toBeNull();
-      expect(_.isString(etag)).toBeTruthy();
-      expect(data.name).toBe('jquery');
-      done();
+  describe('UpStorge::getRemoteMetadata', () => {
+    test('should be get remote metadata', (done) => {
+      const proxy = generateProxy();
+      proxy.getRemoteMetadata('jquery', {}, (err, data, etag) => {
+        expect(err).toBeNull();
+        expect(_.isString(etag)).toBeTruthy();
+        expect(data.name).toBe('jquery');
+        done();
+      });
+    });
+
+    test('should be get remote metadata with etag', (done) => {
+      const proxy = generateProxy();
+      proxy.getRemoteMetadata('jquery', {etag: '123456'}, (err, data, etag) => {
+        expect(err).toBeNull();
+        expect(_.isString(etag)).toBeTruthy();
+        expect(data.name).toBe('jquery');
+        done();
+      });
+    });
+
+    test('should be get remote metadata package does not exist', (done) => {
+      const proxy = generateProxy();
+      proxy.getRemoteMetadata('@verdaccio/fake-package', {etag: '123456'}, (err, data, etag) => {
+        expect(err).not.toBeNull();
+        expect(err.statusCode).toBe(404);
+        expect(err.message).toMatch(/package doesn't exist on uplink/);
+        done();
+      });
     });
   });
+
+
+    describe('UpStorge::fetchTarball', () => {
+      test('should fetch a tarball from uplink', (done) => {
+        const proxy = generateProxy();
+        const tarball:string = 'https://registry.npmjs.org/aaa/-/aaa-0.0.1.tgz';
+        const stream = proxy.fetchTarball(tarball);
+
+        stream.on('error', function(err) {
+          expect(err).toBeNull();
+          done();
+        });
+
+        stream.on('content-length', function(contentLength) {
+          expect(contentLength).toBeDefined();
+          done();
+        });
+
+      });
+    });
 
 });
