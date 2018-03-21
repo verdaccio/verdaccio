0d3cf84c01ecfc4fb09f2c06048d746c6a554055
Ayush Sharma <ayush.sharma@innovaccer.com>
2018-03-21 16:56:27 +0530

    fix(test): replaces LocaleString with date-nfs/format
    fix(test): replaces timezone to UTC
    
    fix(test): updates variable definitions
    
    fix(test): updates time format

--- test/webui/components/PackageSidebar/lastsync.spec.js
+++ test/webui/components/PackageSidebar/lastsync.spec.js
@@ -6,24 +6,20 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import LastSync from '../../../../src/webui/src/components/PackageSidebar/modules/LastSync';
 import { packageMeta } from '../store/packageMeta';
-
-jest.mock(
-  '../../../../src/webui/utils/datetime',
-  () => require('../__mocks__/datetime').default
-);
  
 console.error = jest.fn();
 
-describe.skip('<PackageSidebar /> : <LastSync />', () => {
+describe('<PackageSidebar /> : <LastSync />', () => {
   it('should load the component and check getters: lastUpdate, recentReleases with package data', () => {
     const wrapper = mount(<LastSync packageMeta={packageMeta} />);
     const instance = wrapper.instance();
     const result = [
-      { time: 'Dec 14, 2017, 3:43 PM', version: '2.7.1' },
-      { time: 'Dec 5, 2017, 11:25 PM', version: '2.7.0' },
-      { time: 'Nov 8, 2017, 10:47 PM', version: '2.6.6' }
+      { time: '2017/12/14, 15:43:27', version: '2.7.1' },
+      { time: '2017/12/05, 23:25:06', version: '2.7.0' },
+      { time: '2017/11/08, 22:47:16', version: '2.6.6' }
     ];
-    expect(instance.lastUpdate).toEqual('Dec 14, 2017, 3:43 PM');
+    const lastUpdated = '2017/12/14, 15:43:52';
+    expect(instance.lastUpdate).toEqual(lastUpdated);
     expect(instance.recentReleases).toEqual(result);
   });
 
