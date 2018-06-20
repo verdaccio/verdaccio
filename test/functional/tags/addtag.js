a1439df1b5ddf8535e667e0aa57877f9eee19355
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-06-20 23:14:31 +0200

    refactor: usage constants for http status

--- test/functional/tags/addtag.js
+++ test/functional/tags/addtag.js
@@ -1,10 +1,13 @@
 import {readFile} from '../lib/test.utils';
+import {HTTP_STATUS} from "../../../src/lib/constants";
 
 const readTags = () => readFile('../fixtures/publish.json5');
 
 export default function(server) {
   test('add tag - 404', () => {
-    return server.addTag('testpkg-tag', 'tagtagtag', '0.0.1').status(404).body_error(/no such package/);
+    return server.addTag('testpkg-tag', 'tagtagtag', '0.0.1')
+      .status(HTTP_STATUS.NOT_FOUND)
+      .body_error(/no such package/);
   });
 
   describe('addtag', () => {
@@ -15,28 +18,26 @@ export default function(server) {
           .replace(/__NAME__/g, 'testpkg-tag')
           .replace(/__VERSION__/g, '0.0.1')
         + ')'
-      )).status(201);
+      )).status(HTTP_STATUS.CREATED);
     });
 
-    test('add testpkg-tag', () => {
-      // TODO: ?
-    });
+    test('add testpkg-tag', () => {});
 
-    test('add tag - bad ver', () => {
+    test('should fails on add non semver version tag ', () => {
       return server.addTag('testpkg-tag', 'tagtagtag', '0.0.1-x')
-        .status(404)
+        .status(HTTP_STATUS.NOT_FOUND)
         .body_error(/version doesn't exist/);
     });
 
     test('add tag - bad tag', () => {
       return server.addTag('testpkg-tag', 'tag/tag/tag', '0.0.1-x')
-        .status(403)
+        .status(HTTP_STATUS.FORBIDDEN)
         .body_error(/invalid tag/);
     });
 
     test('add tag - good', () => {
       return server.addTag('testpkg-tag', 'tagtagtag', '0.0.1')
-        .status(201)
+        .status(HTTP_STATUS.CREATED)
         .body_ok(/tagged/);
     });
   });
