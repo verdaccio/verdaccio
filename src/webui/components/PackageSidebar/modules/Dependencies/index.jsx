b0b631af76082c159df5cb92a9c697f30efd42eb
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-07-28 08:45:10 +0200

    chore: add unit test for #857 scenario

--- src/webui/components/PackageSidebar/modules/Dependencies/index.jsx
+++ src/webui/components/PackageSidebar/modules/Dependencies/index.jsx
@@ -7,13 +7,16 @@ import ModuleContentPlaceholder from '../../ModuleContentPlaceholder';
 
 import classes from './style.scss';
 
+export const NO_DEPENDENCIES = 'Zero Dependencies!';
+export const DEP_ITEM_CLASS = 'dependency-item';
+
 const renderDependenciesList = (dependencies, dependenciesList) => {
   return (
     <ul>
       {dependenciesList.map((dependenceName, index) => {
         return (
           <li
-            className="dependency-item"
+            className={DEP_ITEM_CLASS}
             key={index}
             title={`Depend on version: ${dependencies[dependenceName]}`}
           >
@@ -33,7 +36,7 @@ const Dependencies = ({dependencies = {}}) => {
       {dependenciesList.length > 0 ? (
         renderDependenciesList(dependencies, dependenciesList)
       ) : (
-        <ModuleContentPlaceholder text="Zero Dependencies!" />
+        <ModuleContentPlaceholder text={NO_DEPENDENCIES} />
       )}
     </Module>
   );
