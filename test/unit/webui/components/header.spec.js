8ce1168d2f9820b6dabf294ab20108fab59de272
ayusharma <ayush.aceit@gmail.com>
2018-07-30 19:56:11 +0200

    refactor: improves <Header /> component test cases

--- test/unit/webui/components/header.spec.js
+++ test/unit/webui/components/header.spec.js
@@ -3,13 +3,50 @@
  */
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import { Base64 } from 'js-base64';
+import addHours from 'date-fns/add_hours'
 import Header from '../../../../src/webui/components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import storage from '../../../../src/webui/utils/storage';
 
+jest.mock('../../../../src/webui/utils/storage', () => {
+  class LocalStorageMock {
+    constructor() {
+      this.store = {};
+    }
+
+    clear() {
+      this.store = {};
+    }
+
+    getItem(key) {
+      return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+      this.store[key] = value.toString();
+    }
+
+    removeItem(key) {
+      delete this.store[key];
+    }
+  }
+  return new LocalStorageMock();
+});
+
 jest.mock('../../../../src/webui/utils/api', () => ({
   request: require('./__mocks__/api').default.request,
 }));
+
+console.error = jest.fn();
+
+const generateTokenWithTimeRange = (limit = 0) => {
+  const payload = {
+    username: 'verdaccio',
+    exp: Number.parseInt((addHours(new Date(), limit).getTime() / 1000), 10)
+  }
+  return `xxxxxx.${Base64.encode(JSON.stringify(payload))}.xxxxxx`;
+}
 
 describe('<Header /> component shallow', () => {
   let wrapper;
@@ -35,7 +72,7 @@ describe('<Header /> component shallow', () => {
     expect(HeaderWrapper.state()).toEqual(state);
   });
 
-  it('should load verdaccio logo', () => {
+  it('loadLogo - should load verdaccio logo', () => {
     const HeaderWrapper = wrapper.find(Header).dive();
     const { loadLogo } = HeaderWrapper.instance();
 
@@ -45,15 +82,24 @@ describe('<Header /> component shallow', () => {
     });
   });
 
-  it('should toggleLogin modal', () => {
+  it('toggleLoginModal - should toggle login modal', () => {
     const HeaderWrapper = wrapper.find(Header).dive();
     const { toggleLoginModal } = HeaderWrapper.instance();
-
+   
     expect(toggleLoginModal()).toBeUndefined();
     expect(HeaderWrapper.state('showLogin')).toBeTruthy();
   });
 
-  it('should handleInput set state', () => {
+  it('toggleLoginModal - click on login button and cancel button in login dialog', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    const spy = jest.spyOn(HeaderWrapper.instance(), 'toggleLoginModal');
+    HeaderWrapper.find('.header-button-login').simulate('click');
+    HeaderWrapper.find('.cancel-login-button').simulate('click');
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  })
+
+  it('handleInput - should set username and password in state', () => {
     const HeaderWrapper = wrapper.find(Header).dive();
     const handleInput = HeaderWrapper.instance().handleInput;
 
@@ -127,7 +173,109 @@ describe('<Header /> component shallow', () => {
       expect(HeaderWrapper.state('loginError')).toEqual(error);
     });
   });
+
+  it('renderUserActionButton - should show login button', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    const { renderUserActionButton } = HeaderWrapper.instance();
+    expect(renderUserActionButton()).toMatchSnapshot();
+  });
+
+  it('renderUserActionButton - should show users as loggedin', () => {
+    class MockedHeader extends Header {
+      get isTokenExpire() {
+        return false;
+      }
+    }
+    wrapper = shallow(
+      <BrowserRouter>
+        <MockedHeader />
+      </BrowserRouter>);
+    const HeaderWrapper = wrapper.find(MockedHeader).dive();
+    const { renderUserActionButton } = HeaderWrapper.instance();
+    expect(renderUserActionButton()).toMatchSnapshot();
+  });
+
+  it('isTokenExpire - token is not availabe in storage', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    const { isTokenExpire } = HeaderWrapper.instance();
+    expect(isTokenExpire).toBeTruthy();
+  });
+
+  it('isTokenExpire - token is not a valid payload', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    const { isTokenExpire } = HeaderWrapper.instance();
+    storage.setItem('token', 'not_a_valid_token');
+    expect(isTokenExpire).toBeTruthy();
+  });
+
+  it('isTokenExpire - token should not expire in 24 hrs range', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    storage.setItem('token', generateTokenWithTimeRange(24));
+    expect(HeaderWrapper.instance().isTokenExpire).toBeFalsy();
+    storage.removeItem('token');
+  });
+
+  it('isTokenExpire - token should expire for present', () => {
+    const HeaderWrapper = wrapper.find(Header).dive();
+    storage.setItem('token', generateTokenWithTimeRange());
+    expect(HeaderWrapper.instance().isTokenExpire).toBeTruthy();
+    storage.removeItem('token');
+  });
+
+  it('isTokenExpire - token expiration is not available', () => {
+    const generateTokenWithOutExpiration = () => {
+      const payload = {
+        username: 'verdaccio'
+      }
+      return `xxxxxx.${Base64.encode(JSON.stringify(payload))}.xxxxxx`;
+    }
+    const HeaderWrapper = wrapper.find(Header).dive();
+    storage.setItem('token', generateTokenWithOutExpiration());
+    expect(HeaderWrapper.instance().isTokenExpire).toBeTruthy();
+    storage.removeItem('token');
+  })
+
+  it('isTokenExpire - token expiration is not a number', () => {
+    const generateTokenWithExpirationAsString = () => {
+      const payload = {
+        username: 'verdaccio',
+        exp: 'I am not a number'
+      };
+      return `xxxxxx.${Base64.encode(JSON.stringify(payload))}.xxxxxx`;
+    };
+    const HeaderWrapper = wrapper.find(Header).dive();
+    storage.setItem('token', generateTokenWithExpirationAsString());
+    expect(HeaderWrapper.instance().isTokenExpire).toBeTruthy();
+    storage.removeItem('token');
+  });
+
+  it('isTokenExpire - token is not a valid json token', () => {
+    const generateTokenWithExpirationAsString = () => {
+      const payload = { username: 'verdaccio', exp: 'I am not a number' };
+      return `xxxxxx.${Base64.encode(payload)}.xxxxxx`;
+    };
+    const result = [
+      'Invalid token:',
+      SyntaxError('Unexpected token o in JSON at position 1'),
+      'xxxxxx.W29iamVjdCBPYmplY3Rd.xxxxxx'
+    ]
+    storage.setItem('token', generateTokenWithExpirationAsString());
+    wrapper.find(Header).dive().isTokenExpire;
+    expect(console.error).toBeCalledWith(...result);
+    storage.removeItem('token');
+  });
+
+  it('handleLogout - should clear the local stoage', () => {
+    const storageSpy = jest.spyOn(storage, 'clear');
+    const locationSpy = jest.spyOn(window.location, 'reload');
+    const HeaderWrapper = wrapper.find(Header).dive();
+    const { handleLogout } = HeaderWrapper.instance();
+    handleLogout();
+    expect(storageSpy).toHaveBeenCalled();
+    expect(locationSpy).toHaveBeenCalled()
+  });
 });
+
 
 describe('<Header /> snapshot test', () => {
   it('shoud match snapshot', () => {
@@ -139,3 +287,20 @@ describe('<Header /> snapshot test', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
+
+describe('<Header /> snapshot for loggedin user', () => {
+  beforeAll(() => {
+    storage.setItem('token', generateTokenWithTimeRange(24));
+    storage.setItem('username', 'verdaccio');
+  })
+  afterAll(() => {
+    storage.removeItem('token');
+  })
+  it('should match snapshot', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+})
