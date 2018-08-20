26873682b84f08f0fd73702ee7dbb082b0eea67e
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-08-20 16:31:39 +0200

    refactor: add prettier for yaml files (#931)

--- test/unit/api/config.spec.js
+++ test/unit/api/config.spec.js
@@ -62,17 +62,10 @@ const checkDefaultConfPackages = (config) => {
 describe('Config file', () => {
   beforeAll(function() {
 
-    this.config = new Config(parseConfigFile(resolveConf('full')));
+    this.config = new Config(parseConfigFile(resolveConf('default')));
   });
 
   describe('Config file', () => {
-    test('parse full.yaml', () => {
-      const config = new Config(parseConfigFile(resolveConf('full')));
-      checkDefaultUplink(config);
-      expect(config.storage).toBe('./storage');
-      checkDefaultConfPackages(config);
-    });
-
     test('parse docker.yaml', () => {
       const config = new Config(parseConfigFile(resolveConf('docker')));
       checkDefaultUplink(config);
