f6996443e75e4900c920935261c0b1a074424f1f
ayusharma <ayush.aceit@gmail.com>
2018-10-03 14:03:41 +0200

    refactor: improves notify request service tests

--- test/unit/api/notify-request.spec.js
+++ test/unit/api/notify-request.spec.js
@@ -38,14 +38,10 @@ describe('notifyRequest', () => {
     });
 
     const notification = require('../../../src/lib/notify/notify-request');
+    const args = [{ errorMessage: 'bad data' }, 'notify service has thrown an error: @{errorMessage}'];
 
-    try {
-      const response = await notification.notifyRequest(options, content);
-      expect(response).toBeUndefined();
-    } catch (error) {
-      expect(logger.logger.error).toHaveBeenCalled();
-      expect(error).toEqual(API_ERROR.BAD_DATA);
-    }
+    await expect(notification.notifyRequest(options, content)).rejects.toEqual(API_ERROR.BAD_DATA);
+    expect(logger.logger.error).toHaveBeenCalledWith(...args);
   });
 
   test('when notification service throws error with null error value', async () => {
@@ -60,14 +56,10 @@ describe('notifyRequest', () => {
     });
 
     const notification = require('../../../src/lib/notify/notify-request');
+    const args = [{ errorMessage: 'bad data' }, 'notify service has thrown an error: @{errorMessage}'];
 
-    try {
-      const response = await notification.notifyRequest(options, content);
-      expect(response).toBeUndefined();
-    } catch (error) {
-      expect(logger.logger.error).toHaveBeenCalled();
-      expect(error).toEqual(API_ERROR.BAD_DATA);
-    }
+    await expect(notification.notifyRequest(options, content)).rejects.toEqual(API_ERROR.BAD_DATA);
+    expect(logger.logger.error).toHaveBeenCalledWith(...args);
   });
 
   test('when notification is successfully delivered', async () => {
@@ -82,15 +74,12 @@ describe('notifyRequest', () => {
     });
 
     const notification = require('../../../src/lib/notify/notify-request');
+    const infoArgs = [{ content: 'Verdaccio@x.x.x successfully published' }, 'A notification has been shipped: @{content}'];
+    const debugArgs = [{ body: 'Successfully delivered' }, ' body: @{body}'];
 
-    try {
-      const response = await notification.notifyRequest(options, content);
-      expect(response).toEqual('Successfully delivered');
-      expect(logger.logger.info).toHaveBeenCalled();
-      expect(logger.logger.debug).toHaveBeenCalled();
-    } catch (error) {
-      expect(error).toBeUndefined();
-    }
+    await expect(notification.notifyRequest(options, content)).resolves.toEqual('Successfully delivered');
+    expect(logger.logger.info).toHaveBeenCalledWith(...infoArgs);
+    expect(logger.logger.debug).toHaveBeenCalledWith(...debugArgs);
   });
 
   test('when notification is successfully delivered but body is undefined/null', async () => {
@@ -104,12 +93,9 @@ describe('notifyRequest', () => {
     });
 
     const notification = require('../../../src/lib/notify/notify-request');
+    const infoArgs = [{ content: 'Verdaccio@x.x.x successfully published' }, 'A notification has been shipped: @{content}'];
 
-    try {
-      await notification.notifyRequest(options, content);
-    } catch (error) {
-      expect(logger.logger.info).toHaveBeenCalled();
-      expect(error.message).toEqual('body is missing');
-    }
+    await expect(notification.notifyRequest(options, content)).rejects.toThrowError('body is missing');
+    expect(logger.logger.info).toHaveBeenCalledWith(...infoArgs);
   });
 });
