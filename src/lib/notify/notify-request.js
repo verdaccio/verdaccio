83b586f000eff14d04a493478de54f47020c8d05
Ayush Sharma <ayush.aceit@gmail.com>
2018-10-03 23:32:04 +0200

    refactor: tests for notify request service (#1039)
    * refactor: tests for notify request service
    
    * refactor: improves notify request service tests
    
    * refactor: uses beforeEach for jest.resetModules

--- src/lib/notify/notify-request.js
+++ src/lib/notify/notify-request.js
@@ -13,17 +13,13 @@ export function notifyRequest(options, content) {
       if (err || response.statusCode >= HTTP_STATUS.BAD_REQUEST) {
         const errorMessage = isNil(err) ? response.body : err.message;
         logger.logger.error({ errorMessage }, 'notify service has thrown an error: @{errorMessage}');
-
         reject(errorMessage);
       } else {
         logger.logger.info({ content }, 'A notification has been shipped: @{content}');
         if (isNil(body) === false) {
-          const bodyResolved = isNil(body) === false ? body : null;
-
           logger.logger.debug({ body }, ' body: @{body}');
-          return resolve(bodyResolved);
+          resolve(body);
         }
-
         reject(Error('body is missing'));
       }
     });
