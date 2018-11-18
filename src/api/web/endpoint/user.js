712db31a43f0b91823282d53d09b71bf041d97fc
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-11-18 18:41:05 +0100

    feat: endpoints change password on ui (#1068)
    * test: split up web and endpoints api
    
    * chore: add reset password endpoint
    
    not fully implemented yet
    
    * feat: add password change api ednpoint for web

--- src/api/web/endpoint/user.js
+++ src/api/web/endpoint/user.js
@@ -3,13 +3,14 @@
  * @flow
  */
 
-import { HTTP_STATUS } from '../../../lib/constants';
+import _ from 'lodash';
+import { API_ERROR, APP_ERROR, HTTP_STATUS } from '../../../lib/constants';
 
 import type { Router } from 'express';
 import type { Config, RemoteUser, JWTSignOptions } from '@verdaccio/types';
 import type { IAuth, $ResponseExtend, $RequestExtend, $NextFunctionVer } from '../../../../types';
 import { ErrorCode } from '../../../lib/utils';
-import { getSecurity } from '../../../lib/auth-utils';
+import { getSecurity, validatePassword } from '../../../lib/auth-utils';
 
 function addUserAuthApi(route: Router, auth: IAuth, config: Config) {
   route.post('/login', function(req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer) {
@@ -29,6 +30,33 @@ function addUserAuthApi(route: Router, auth: IAuth, config: Config) {
         });
       }
     });
+  });
+
+  route.put('/reset_password', function(req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer) {
+    if (_.isNil(req.remote_user.name)) {
+      res.status(HTTP_STATUS.UNAUTHORIZED);
+      return next({
+        // FUTURE: update to a more meaningful message
+        message: API_ERROR.MUST_BE_LOGGED,
+      });
+    }
+
+    const { password } = req.body;
+    const { name } = req.remote_user;
+
+    if (validatePassword(password.new) === false) {
+      auth.changePassword(name, password.old, password.new, (err, isUpdated) => {
+        if (_.isNil(err) && isUpdated) {
+          next({
+            ok: true,
+          });
+        } else {
+          return next(ErrorCode.getInternalError(API_ERROR.INTERNAL_SERVER_ERROR));
+        }
+      });
+    } else {
+      return next(ErrorCode.getCode(HTTP_STATUS.BAD_REQUEST, APP_ERROR.PASSWORD_VALIDATION));
+    }
   });
 }
 
--- test/unit/api/mock.js
+++ test/unit/api/mock.js
@@ -11,7 +11,7 @@ export function mockServer(port: number) {
   const pathStore = path.join(__dirname, '../partials');
   const verdaccioConfig = new VerdaccioConfig(
     path.join(pathStore, '/mock-store'),
-    path.join(pathStore, '/config-unit-test.yaml'), `http://${DOMAIN_SERVERS}:${port}/`, port);
+    path.join(pathStore, '/config-unit-mock-server-test.yaml'), `http://${DOMAIN_SERVERS}:${port}/`, port);
   const server: IServerBridge = new Server(verdaccioConfig.domainPath);
 
   return new VerdaccioProcess(verdaccioConfig, server, false, false, false);
