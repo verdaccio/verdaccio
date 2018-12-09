6cface40ccec338b877d449ae394b99ef8175791
Priscila Oliveira <priscila.oliveira@eversports.com>
2018-12-10 00:31:45 +0100

    refactor: organized modifiers

--- src/webui/components/Header/styles.js
+++ src/webui/components/Header/styles.js
@@ -11,28 +11,11 @@ import IconButton from '@material-ui/core/IconButton/index';
 import colors from '../../utils/styles/colors';
 import mq from '../../utils/styles/media';
 
-export const NavBar = styled(AppBar)`
-  && {
-    background-color: ${colors.primary};
-    min-height: 60px;
-    display: flex;
-    justify-content: center;
-  }
-`;
-
 export const InnerNavBar = styled(Toolbar)`
   && {
     justify-content: space-between;
     align-items: center;
     padding: 0 15px;
-    ${mq.large(css`
-      padding: 0 20px;
-    `)};
-    ${mq.xlarge(css`
-      max-width: 1240px;
-      width: 100%;
-      margin: 0 auto;
-    `)};
   }
 `;
 
@@ -62,9 +45,6 @@ export const MobileNavBar = styled.div`
     border-bottom: 1px solid ${colors.greyLight};
     padding: 8px;
     position: relative;
-    ${mq.medium(css`
-      display: none;
-    `)};
   }
 `;
 
@@ -82,9 +62,6 @@ export const InnerMobileNavBar = styled.div`
 export const IconSearchButton = styled(IconButton)`
   && {
     display: block;
-    ${mq.medium(css`
-      display: none;
-    `)};
   }
 `;
 
@@ -94,8 +71,37 @@ export const SearchWrapper = styled.div`
     max-width: 393px;
     width: 100%;
     display: none;
+  }
+`;
+
+export const NavBar = styled(AppBar)`
+  && {
+    background-color: ${colors.primary};
+    min-height: 60px;
+    display: flex;
+    justify-content: center;
     ${mq.medium(css`
-      display: flex;
+      ${SearchWrapper} {
+        display: flex;
+      }
+      ${IconSearchButton} {
+        display: none;
+      }
+      ${MobileNavBar} {
+        display: none;
+      }
+    `)};
+    ${mq.large(css`
+      ${InnerNavBar} {
+        padding: 0 20px;
+      }
+    `)};
+    ${mq.xlarge(css`
+      ${InnerNavBar} {
+        max-width: 1240px;
+        width: 100%;
+        margin: 0 auto;
+      }
     `)};
   }
 `;
