ba9e48e5b141e7473fb11f7c93b4236d9080e350
Priscila Oliveira <priscila.oliveira@eversports.com>
2019-01-06 11:11:10 +0100

    refactor: actived the rule react/jsx-one-expression-per-line

--- src/webui/components/NotFound/index.js
+++ src/webui/components/NotFound/index.js
@@ -9,7 +9,11 @@ import { IProps } from './types';
 
 const NotFound = ({ pkg }: IProps) => (
   <Wrapper>
-    <h1>Error 404 -{pkg}</h1>
+    <h1>
+      // TODO. Resolved the rule react/jsx-one-expression-per-line for now, but in the future we are going to use i18n here
+      {'Error 404 -'}
+      {pkg}
+    </h1>
     <hr />
     <p>Oops, The package you are trying to access does not exist.</p>
   </Wrapper>
