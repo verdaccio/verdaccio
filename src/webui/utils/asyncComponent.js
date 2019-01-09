4f41fc94e271f1339de21a2fbb30884245f09316
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-01-09 23:41:34 +0100

    fix: broken home page

--- src/webui/utils/asyncComponent.js
+++ src/webui/utils/asyncComponent.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
-export function asyncComponent(getComponentFunc) {
+export function asyncComponent(getComponent) {
   return class AsyncComponent extends React.Component {
     static Component = null;
-    state = {Component: this.getComponent()};
+    state = {Component: AsyncComponent.Component};
 
-    getComponent() {
-      if (!AsyncComponent.Component) {
-        getComponentFunc().then(({default: Component}) => {
+    componentDidMount() {
+      const {Component} = this.state;
+
+      if (!Component) {
+        getComponent().then(({default: Component}) => {
           AsyncComponent.Component = Component;
-          return Component;
+          /* eslint react/no-did-mount-set-state:0 */
+          this.setState({Component});
         });
       }
-
-      return AsyncComponent.Component;
     }
-    
     render() {
       const {Component} = this.state;
       if (Component) {
         return <Component {...this.props} />;
       }
+
       return null;
     }
   };
