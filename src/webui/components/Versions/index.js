e9168a8a30fa4732e4a839e68608f944787836af
Priscila Oliveira <priscila.oliveira@eversports.com>
2019-01-20 12:02:02 +0100

    refactor: added new style

--- src/webui/components/Versions/index.js
+++ src/webui/components/Versions/index.js
@@ -3,37 +3,49 @@
  * @flow
  */
 
-import React from 'react';
-import List from '@material-ui/core/List/index';
-import ListItem from '@material-ui/core/ListItem/index';
-
 import { DetailContextConsumer } from '../../pages/version/index';
+import { formatDateDistance } from '../../utils/package';
+import { Heading, Spacer, ListItemText } from './styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import React from 'react';
 
-class Versions extends React.Component<any, any> {
+class Versions extends React.PureComponent {
   render() {
-    return (
-      // $FlowFixMe
-      <DetailContextConsumer>
-        {({ packageMeta }) => {
-          const { versions } = packageMeta;
-          if (versions) {
-            return this.renderVersions(versions);
-          }
-        }}
-      </DetailContextConsumer>
-    );
+    return <DetailContextConsumer>{({ packageMeta }) => this.renderContent(packageMeta['dist-tags'], packageMeta.versions)}</DetailContextConsumer>;
   }
+
+  renderPackageList = (packages: object, isVersion: boolean = false) => (
+    <List>
+      {Object.keys(packages)
+        .reverse()
+        .map(version => (
+          <ListItem key={version}>
+            <ListItemText>{version}</ListItemText>
+            <Spacer />
+            <ListItemText>{isVersion ? `${formatDateDistance('2017-10-26T09:03:15.044Z')} ago` : packages[version]}</ListItemText>
+          </ListItem>
+        ))}
+    </List>
+  );
 
   // $FlowFixMe
-  renderVersions(versions) {
-    return (
-      <List>
-        {Object.keys(versions).map(version => (
-          <ListItem key={version}>{versions[version].name}</ListItem>
-        ))}
-      </List>
-    );
-  }
+  renderContent = (distTags, versions) => (
+    <>
+      {distTags && (
+        <>
+          <Heading variant={'subheading'}>{'Current Tags'}</Heading>
+          {this.renderPackageList(distTags)}
+        </>
+      )}
+      {versions && (
+        <>
+          <Heading variant={'subheading'}>{'Version History'}</Heading>
+          {this.renderPackageList(versions, true)}
+        </>
+      )}
+    </>
+  );
 }
 
 export default Versions;
