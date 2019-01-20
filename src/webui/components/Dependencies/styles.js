05498fe651013defa3a546e317e0de4c0be69c65
Priscila Oliveira <priscila.oliveira@eversports.com>
2019-01-20 13:32:08 +0100

    refactor: added renderList

--- src/webui/components/Dependencies/styles.js
+++ src/webui/components/Dependencies/styles.js
@@ -4,7 +4,10 @@
  */
 
 import styled from 'react-emotion';
-import Card from '@material-ui/core/Card/index';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { default as MuiListItem } from '@material-ui/core/ListItem';
+import { default as MuiCardContent } from '@material-ui/core/CardContent';
 
 export const Content = styled.div`
   && {
@@ -12,22 +15,27 @@ export const Content = styled.div`
   }
 `;
 
-export const Tags = styled.span`
+export const CardContent = styled(MuiCardContent)`
   && {
-    display: flex;
-    justify-content: start;
-    flex-wrap: wrap;
-  }
-`;
-
-export const Tag = styled.span`
-  && {
-    margin: 5px;
+    padding-bottom: 0;
   }
 `;
 
 export const CardWrap = styled(Card)`
   && {
     margin: 0 0 25px;
+  }
+`;
+
+export const Heading = styled(Typography)`
+  && {
+    font-weight: 700;
+  }
+`;
+
+export const ListItem = styled(MuiListItem)`
+  && {
+    padding-left: 0;
+    padding-right: 0;
   }
 `;
