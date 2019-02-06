e3fa6d37a6c0c0305f99ec531d6225941f648849
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-02-06 07:23:26 +0100

    chore: remove debug code

--- src/webui/history.js
+++ src/webui/history.js
@@ -6,10 +6,4 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
-// Listen for changes to the current location.
-history.listen((location, action) => {
-  // location is an object like window.location
-  console.log('====>', action, location.pathname, location.state);
-});
-
 export default history;
