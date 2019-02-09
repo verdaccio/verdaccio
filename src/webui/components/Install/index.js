1d6efd79d28b6c5ed64680424061c2ca5ddd72f6
Ayush Sharma <ayush.aceit@gmail.com>
2019-02-09 22:56:14 +0100

    refactor: adds logo for npm, pnpm and yarn

--- src/webui/components/Install/index.js
+++ src/webui/components/Install/index.js
@@ -1,53 +1,58 @@
+/**
+ * @prettier
+ */
+
 import React, { Component } from 'react';
 
-import List from '@material-ui/core/List';
-import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List/index';
+import ListItemText from '@material-ui/core/ListItemText/index';
 
 import { DetailContextConsumer } from '../../pages/version/index';
 import CopyToClipBoard from '../CopyToClipBoard';
 
 import { Heading, InstallItem, PackageMangerAvatar } from './styles';
+// logos of package managers
+import npm from './img/npm.svg';
+import pnpm from './img/pnpm.svg';
+import yarn from './img/yarn.svg';
 
 class Install extends Component {
   render() {
     return (
       <DetailContextConsumer>
-        {(context) => {
+        {context => {
           return this.renderCopyCLI(context);
         }}
       </DetailContextConsumer>
     );
-  };
+  }
 
   renderCopyCLI = ({ packageName }) => {
     return (
       <>
-        <List subheader={<Heading variant={"subheading"}>{'Installation'}</Heading>}>
-          {this.renderListItems(packageName)}
-        </List>
+        <List subheader={<Heading variant={'subheading'}>{'Installation'}</Heading>}>{this.renderListItems(packageName)}</List>
       </>
     );
-  }
+  };
 
-  renderListItems = (packageName) => {
+  renderListItems = packageName => {
     return (
       <>
         <InstallItem>
-          <PackageMangerAvatar alt={"npm logo"} src={"https://cldup.com/Rg6WLgqccB.svg"} />
+          <PackageMangerAvatar alt={'npm logo'} src={npm} />
           <ListItemText primary={<CopyToClipBoard text={`npm install ${packageName}`} />} secondary={'Install using NPM'} />
         </InstallItem>
         <InstallItem>
-          <PackageMangerAvatar alt={"yarn logo"} src={"https://raw.githubusercontent.com/yarnpkg/assets/master/yarn-kitten-circle.png"} />
+          <PackageMangerAvatar alt={'yarn logo'} src={yarn} />
           <ListItemText primary={<CopyToClipBoard text={`yarn add ${packageName}`} />} secondary={'Install using Yarn'} />
         </InstallItem>
         <InstallItem>
-          <PackageMangerAvatar alt={"pnpm logo"} src={"https://pnpm.js.org/img/pnpm-no-name-with-frame.svg"} />
+          <PackageMangerAvatar alt={'pnpm logo'} src={pnpm} />
           <ListItemText primary={<CopyToClipBoard text={`pnpm install ${packageName}`} />} secondary={'Install using PNPM'} />
         </InstallItem>
       </>
     );
-  }
-  
+  };
 }
 
 export default Install;
