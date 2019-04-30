11a48e56d501ac672f861957aa839c946acfcb6a
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-04-30 08:50:43 +0200

    feat: add some security headers for web UI
    The idea behind this is have more control over the content is rendered mostly via README.

--- src/api/middleware.js
+++ src/api/middleware.js
@@ -22,9 +22,15 @@ export function match(regexp: RegExp) {
   };
 }
 
-export function securityIframe(req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer) {
+export function securityWebHeadersSecurity(req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer) {
   // disable loading in frames (clickjacking, etc.)
-  res.header('X-Frame-Options', 'deny');
+  res.header(HEADERS.XFRAMES_OPTIONS, 'deny');
+  // avoid stablish connections outside of domain
+  res.header(HEADERS.CSP, "connect-src 'self'");
+  // https://stackoverflow.com/questions/18337630/what-is-x-content-type-options-nosniff
+  res.header(HEADERS.CTO, 'nosniff');
+  // https://stackoverflow.com/questions/9090577/what-is-the-http-header-x-xss-protection
+  res.header(HEADERS.XSS, '1; mode=block');
   next();
 }
 
