82637d7b12e27a82ce2fe9dbb1a3db2d835500eb
AllenBird <15858208586@163.com>
2019-07-12 10:59:33 +0800

    Update local-storage.js
    ** Type: bug **
    
    ** Description: ** 
    Delete versions and history in package.json, to fix issue #1359.
    https://github.com/verdaccio/verdaccio/issues/1359

--- src/lib/local-storage.js
+++ src/lib/local-storage.js
@@ -360,6 +360,15 @@ class LocalStorage implements IStorage {
       name,
       (data, cb) => {
         if (data._attachments[filename]) {
+          // get version form _attachments;
+          // there aways had 'version' in verdaccio publish package.
+          // but there was no 'version' in package witch unlink form npmjs
+          // see https://github.com/verdaccio/verdaccio/issues/1359
+          const version = data._attachments[filename].version;
+          if (version && data.versions[version] && data.time[version]) {
+            delete data.versions[version];
+            delete data.time[version];
+          }
           delete data._attachments[filename];
           cb();
         } else {
