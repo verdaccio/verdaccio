af7885be5cdf295793dc429608bfea53585d788c
Raido Kuli <raido357@gmail.com>
2019-08-01 17:14:56 +0300

    Backport #1409 dash in package name fix

--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -8,6 +8,7 @@ describe('Validate', function() {
     assert( validate('verdaccio') );
     assert( validate('some.weird.package-zzz') );
     assert( validate('old-package@0.1.2.tgz') );
+    assert( validate('-build-infra') );
   });
 
   it('uppercase', function() {
