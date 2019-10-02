19065ccaa53f2a35067fbcd1a831e2d8aebc8f52
polemius <ivan.menshykov@gmail.com>
2019-10-02 19:50:51 +0200

    fix: fixing the lint issues

--- src/api/endpoint/api/ping.ts
+++ src/api/endpoint/api/ping.ts
@@ -6,7 +6,7 @@
 import { Router } from 'express';
 import { $RequestExtend, $ResponseExtend, $NextFunctionVer } from '../../../../types';
 
-export default function(route: Router) {
+export default function(route: Router): void {
   route.get('/-/ping', function(req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer) {
     next({});
   });
