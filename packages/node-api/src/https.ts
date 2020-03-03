e2c6408b561a112c8b349a1b054c05ad4cd88407
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-03-03 23:59:19 +0100

    feat: migrate yarn workspaces (#1546)

---
+++ packages/node-api/src/https.ts
@@ -0,0 +1,34 @@
+import { API_ERROR, certPem, csrPem, keyPem } from '@verdaccio/dev-commons';
+
+import {resolveConfigPath} from "./cli-utils";
+
+const logger = require('@verdaccio/logger');
+
+export function logHTTPSWarning(storageLocation) {
+	logger.logger.fatal(
+		[
+			'You have enabled HTTPS and need to specify either ',
+			'    "https.key", "https.cert" and "https.ca" or ',
+			'    "https.pfx" and optionally "https.passphrase" ',
+			'to run https server',
+			'',
+			// commands are borrowed from node.js docs
+			'To quickly create self-signed certificate, use:',
+			' $ openssl genrsa -out ' + resolveConfigPath(storageLocation, keyPem) + ' 2048',
+			' $ openssl req -new -sha256 -key ' + resolveConfigPath(storageLocation, keyPem) + ' -out ' + resolveConfigPath(storageLocation, csrPem),
+			' $ openssl x509 -req -in ' +
+			resolveConfigPath(storageLocation, csrPem) +
+			' -signkey ' +
+			resolveConfigPath(storageLocation, keyPem) +
+			' -out ' +
+			resolveConfigPath(storageLocation, certPem),
+			'',
+			'And then add to config file (' + storageLocation + '):',
+			'  https:',
+			`    key: ${resolveConfigPath(storageLocation, keyPem)}`,
+			`    cert: ${resolveConfigPath(storageLocation, certPem)}`,
+			`    ca: ${resolveConfigPath(storageLocation, csrPem)}`,
+		].join('\n')
+	);
+	process.exit(2);
+}
---
+++ packages/web/test/utils.spec.ts
@@ -0,0 +1,217 @@
+import { addGravatarSupport } from '../src/web-utils';
+import {  generateGravatarUrl, GENERIC_AVATAR } from '../src/user';
+
+describe('Utilities', () => {
+  describe('User utilities', () => {
+    test('should generate gravatar url with email', () => {
+      const gravatarUrl: string = generateGravatarUrl('user@verdaccio.org');
+
+      expect(gravatarUrl).toMatch('https://www.gravatar.com/avatar/');
+      expect(gravatarUrl).not.toMatch('000000000');
+    });
+
+    test('should generate generic gravatar url', () => {
+      const gravatarUrl: string = generateGravatarUrl();
+
+      expect(gravatarUrl).toMatch(GENERIC_AVATAR);
+    });
+  });
+
+  describe('addGravatarSupport', () => {
+    test('check for blank object', () => {
+      // @ts-ignore
+      expect(addGravatarSupport({})).toEqual({});
+    });
+
+    test('author, contributors and maintainers fields are not present', () => {
+      const packageInfo = {
+        latest: {}
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(packageInfo);
+    });
+
+    test('author field is a blank object', () => {
+      const packageInfo = { latest: { author: {} } };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(packageInfo);
+    });
+
+    test('author field is a string type', () => {
+      const packageInfo = {
+        latest: { author: 'user@verdccio.org' }
+      };
+      const result = {
+        latest: {
+          author: {
+            author: 'user@verdccio.org',
+            avatar: GENERIC_AVATAR,
+            email: ''
+          }
+        }
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(result);
+    });
+
+    test('author field is an object type with author information', () => {
+      const packageInfo = {
+        latest: { author: { name: 'verdaccio', email: 'user@verdccio.org' } }
+      };
+      const result = {
+        latest: {
+          author: {
+            avatar:
+              'https://www.gravatar.com/avatar/794d7f6ef93d0689437de3c3e48fadc7',
+            email: 'user@verdccio.org',
+            name: 'verdaccio'
+          }
+        }
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(result);
+    });
+
+    test('contributor field is a blank array', () => {
+      const packageInfo = {
+        latest: {
+          contributors: []
+        }
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(packageInfo);
+    });
+
+    describe("contributors", () => {
+      test('contributors field has contributors', () => {
+        const packageInfo = {
+          latest: {
+            contributors: [
+              { name: 'user', email: 'user@verdccio.org' },
+              { name: 'user1', email: 'user1@verdccio.org' }
+            ]
+          }
+        };
+
+        const result = {
+          latest: {
+            contributors: [
+              {
+                avatar:
+                  'https://www.gravatar.com/avatar/794d7f6ef93d0689437de3c3e48fadc7',
+                email: 'user@verdccio.org',
+                name: 'user'
+              },
+              {
+                avatar:
+                  'https://www.gravatar.com/avatar/51105a49ce4a9c2bfabf0f6a2cba3762',
+                email: 'user1@verdccio.org',
+                name: 'user1'
+              }
+            ]
+          }
+        };
+
+        // @ts-ignore
+        expect(addGravatarSupport(packageInfo)).toEqual(result);
+      });
+
+      test('contributors field is an object', () => {
+        const packageInfo = {
+          latest: {
+            contributors: { name: 'user', email: 'user@verdccio.org' }
+          }
+        };
+
+        const result = {
+          latest: {
+            contributors: [
+              {
+                avatar: 'https://www.gravatar.com/avatar/794d7f6ef93d0689437de3c3e48fadc7',
+                email: 'user@verdccio.org',
+                name: 'user'
+              }
+            ]
+          }
+        };
+
+        // @ts-ignore
+        expect(addGravatarSupport(packageInfo)).toEqual(result);
+      });
+
+      test('contributors field is a string', () => {
+        const contributor = 'Barney Rubble <b@rubble.com> (http://barnyrubble.tumblr.com/)';
+        const packageInfo = {
+          latest: {
+            contributors: contributor
+          }
+        };
+
+        const result = {
+          latest: {
+            contributors: [
+              {
+                avatar: GENERIC_AVATAR,
+                email: contributor,
+                name: contributor
+              }
+            ]
+          }
+        };
+
+        // @ts-ignore
+        expect(addGravatarSupport(packageInfo)).toEqual(result);
+      });
+    });
+
+    test('maintainers field is a blank array', () => {
+      const packageInfo = {
+        latest: {
+          maintainers: []
+        }
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(packageInfo);
+    });
+
+    test('maintainers field has maintainers', () => {
+      const packageInfo = {
+        latest: {
+          maintainers: [
+            { name: 'user', email: 'user@verdccio.org' },
+            { name: 'user1', email: 'user1@verdccio.org' }
+          ]
+        }
+      };
+
+      const result = {
+        latest: {
+          maintainers: [
+            {
+              avatar:
+                'https://www.gravatar.com/avatar/794d7f6ef93d0689437de3c3e48fadc7',
+              email: 'user@verdccio.org',
+              name: 'user'
+            },
+            {
+              avatar:
+                'https://www.gravatar.com/avatar/51105a49ce4a9c2bfabf0f6a2cba3762',
+              email: 'user1@verdccio.org',
+              name: 'user1'
+            }
+          ]
+        }
+      };
+
+      // @ts-ignore
+      expect(addGravatarSupport(packageInfo)).toEqual(result);
+    });
+  });
+
+});
