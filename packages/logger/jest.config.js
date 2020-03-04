6b062c12aec8a325b226926166b6e693703d6e13
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-03-04 22:36:43 +0100

    chore: reuse jest config

--- packages/logger/jest.config.js
+++ packages/logger/jest.config.js
@@ -1,9 +1,6 @@
-module.exports = {
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  verbose: true,
-  collectCoverage: false,
-  coveragePathIgnorePatterns: ['node_modules', 'fixtures'],
-};
+const config = require('../../jest/config');
+
+module.exports = Object.assign({}, config, {
+  collectCoverage: false
+});
+
