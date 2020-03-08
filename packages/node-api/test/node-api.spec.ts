8b0c77ac03da05740030efcbc15322daebc02056
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-03-08 09:19:12 +0100

    refactor: utilities and auth

--- packages/node-api/test/node-api.spec.ts
+++ packages/node-api/test/node-api.spec.ts
@@ -1,14 +1,12 @@
 import path from 'path';
-import _ from 'lodash';
 import selfsigned from 'selfsigned';
 import os from 'os';
 import fs from 'fs';
 
 import { configExample } from '@verdaccio/mock';
-import {DEFAULT_DOMAIN, DEFAULT_PORT, DEFAULT_PROTOCOL} from '@verdaccio/dev-commons';
+import {DEFAULT_DOMAIN, DEFAULT_PROTOCOL} from '@verdaccio/dev-commons';
 import {parseConfigFile} from '@verdaccio/utils';
 
-import { getListListenAddresses } from '../src/cli-utils';
 import { logger } from '@verdaccio/logger';
 
 import { startVerdaccio } from '../src';
@@ -181,66 +179,4 @@ describe('startServer via API', () => {
     });
 
   });
-
-  describe('getListListenAddresses test', () => {
-
-    test('should return no address if a single address is wrong', () => {
-      // @ts-ignore
-      const addrs = getListListenAddresses("wrong");
-
-      expect(_.isArray(addrs)).toBeTruthy();
-      expect(addrs).toHaveLength(0);
-    });
-
-    test('should return no address if a two address are wrong', () => {
-      // @ts-ignore
-      const addrs = getListListenAddresses(["wrong", "same-wrong"]);
-
-      expect(_.isArray(addrs)).toBeTruthy();
-      expect(addrs).toHaveLength(0);
-    });
-
-    test('should return a list of 1 address provided', () => {
-      // @ts-ignore
-      const addrs = getListListenAddresses(null, '1000');
-
-      expect(_.isArray(addrs)).toBeTruthy();
-      expect(addrs).toHaveLength(1);
-    });
-
-    test('should return a list of 2 address provided', () => {
-      // @ts-ignore
-      const addrs = getListListenAddresses(null, ['1000', '2000']);
-
-      expect(_.isArray(addrs)).toBeTruthy();
-      expect(addrs).toHaveLength(2);
-    });
-
-    test(`should return by default ${DEFAULT_PORT}`, () => {
-      // @ts-ignore
-      const [addrs] = getListListenAddresses();
-
-      // @ts-ignore
-      expect(addrs.proto).toBe(DEFAULT_PROTOCOL);
-      // @ts-ignore
-      expect(addrs.host).toBe(DEFAULT_DOMAIN);
-      // @ts-ignore
-      expect(addrs.port).toBe(DEFAULT_PORT);
-    });
-
-    test('should return default proto, host and custom port', () => {
-      const initPort = '1000';
-      // @ts-ignore
-      const [addrs] = getListListenAddresses(null, initPort);
-
-      // @ts-ignore
-      expect(addrs.proto).toEqual(DEFAULT_PROTOCOL);
-      // @ts-ignore
-      expect(addrs.host).toEqual(DEFAULT_DOMAIN);
-      // @ts-ignore
-      expect(addrs.port).toEqual(initPort);
-    });
-
-  });
-
 });
