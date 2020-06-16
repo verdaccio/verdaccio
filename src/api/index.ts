04bb121f54a35e20d56b984c847c5c9e599cb93f
hydra13 <rob.sabirov@yandex.ru>
2020-06-16 19:22:34 +0300

    refactor: move bodyParse to the upper level

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
 import { HttpError } from 'http-errors';
@@ -45,6 +46,8 @@ const defineAPI = function(config: IConfig, storage: IStorageHandler): any {
   if (config._debug) {
     hookDebug(app, config.self_path);
   }
+
+  app.use(bodyParser.json({ strict: false, limit: config.max_body_size || '10mb' }));
 
   // register middleware plugins
   const plugin_params = {
