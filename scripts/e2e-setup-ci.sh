40480f226c0d58ef036d238a53d8a1d33024b278
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-06-17 19:47:10 +0200

    chore: test e2e

---
+++ scripts/e2e-setup-ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+HERE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+TEMP_DIR="$(mktemp -d)"
+
+cd "${TEMP_DIR}"
+
+echo $TEMP_DIR
+echo $HERE_DIR
