012689305585d9da95f2e2ae22f8bd4fd056d170
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-08-13 23:36:23 +0200

    chore: extend lint rules to root

--- packages/store/jest.config.js
+++ packages/store/jest.config.js
@@ -2,6 +2,5 @@ const config = require('../../jest/config');
 
 module.exports = Object.assign({}, config, {
   // FIXME: coverage fails here
-  collectCoverage: true
+  collectCoverage: true,
 });
-
