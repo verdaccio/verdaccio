0073bb92ae15ffb873d206132022adfd6ba9d0f6
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-08-27 22:32:05 +0200

    build: introduce preconstruct

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 10,
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    'babel-plugin-dynamic-import-node',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
+  ignore: ['**/*.d.ts'],
+};
