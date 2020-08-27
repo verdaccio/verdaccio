b10b1861a2b0319ef791959615c60585b36c65ca
Juan Picado <juanpicado19@gmail.com>
2020-08-27 21:23:56 +0200

    chore: restore website build (#1920)

--- packages/hooks/test/notify.spec.ts
+++ packages/hooks/test/notify.spec.ts
@@ -55,6 +55,7 @@ describe('Notifications:: Notify', () => {
   });
 
   test('should send multiple notification', async () => {
+    const name = 'package';
     // @ts-ignore
     await notify({ name }, multiNotificationConfig, { name: 'foo' }, 'bar');
 
