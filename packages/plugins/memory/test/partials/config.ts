4a252873b75a9b8bb8f98bbe1398659ff2271898
Juan Picado <juanpicado19@gmail.com>
2020-10-23 22:05:46 +0200

    refactor: relocate verdaccio-memory plugin (#1974)

---
+++ packages/plugins/memory/test/partials/config.ts
@@ -0,0 +1,50 @@
+import { Config } from '@verdaccio/types';
+
+const config: Config = {
+  user_agent: 'string',
+  server_id: 1234,
+  secret: '12345',
+  self_path: './nowhere',
+  uplinks: {
+    npmjs: {
+      url: 'https://registry.npmjs.org/',
+    },
+  },
+  security: {
+    web: {
+      sign: {},
+      verify: {},
+    },
+    api: {
+      legacy: true,
+    },
+  },
+  packages: {
+    test: {
+      storage: '',
+      publish: [''],
+      proxy: [''],
+      access: [''],
+    },
+  },
+  web: {
+    enable: true,
+    title: 'string',
+    logo: 'string',
+  },
+  logs: [],
+  auth: {},
+  notifications: {
+    method: '',
+    packagePattern: /a/,
+    packagePatternFlags: '',
+    headers: {},
+    endpoint: '',
+    content: '',
+  },
+  checkSecretKey: () => '1234',
+  getMatchedPackagesSpec: jest.fn(),
+  hasProxyTo: () => false,
+};
+
+export default config;
