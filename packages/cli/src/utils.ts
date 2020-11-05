71f5ef2d5971223d0511ce00f1e6c55b2c965400
Juan Picado <juanpicado19@gmail.com>
2020-11-05 23:22:09 +0100

    refactor: config module (#1995)
    * refactor: clean up config
    
    * refactor: config, move methods
    
    * chore: remove js-yaml from utils

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,6 +1,6 @@
 import semver from 'semver';
 
-export const MIN_NODE_VERSION = '6.9.0';
+export const MIN_NODE_VERSION = '10.22.1';
 
 export const isVersionValid = () =>
   semver.satisfies(process.version, `>=${MIN_NODE_VERSION}`) === false;
