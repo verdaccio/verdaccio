fe60eab99c8ffd2c0acc40baada8ca039929eeca
Juan Picado <juanpicado19@gmail.com>
2020-12-21 10:22:06 +0100

    enable CLI E2e on 5.x (#2011)
    * test: enable e2e
    
    * test: improve setup
    
    * chore: update setup
    
    * test: publish on temp folder
    
    * chore: initial setup example
    
    * chore: add global install to pnpm
    
    * chore: update test script
    
    * test: add info command
    
    * chore: add install tests
    
    * chore: add debug enabled code
    
    * chore: update pnpm lock file

--- test/e2e-cli/setup/test_environment.ts
+++ test/e2e-cli/setup/test_environment.ts
@@ -1,8 +1,12 @@
 import os from 'os';
 import path from 'path';
+import buildDebug from 'debug';
 import NodeEnvironment from 'jest-environment-node';
 const fs = require('fs');
+
 const __global = require('../utils/global');
+
+const debug = buildDebug('verdaccio:e2e:env');
 
 class E2ECliTestEnvironment extends NodeEnvironment {
   constructor(config) {
@@ -10,15 +14,20 @@ class E2ECliTestEnvironment extends NodeEnvironment {
   }
 
   async setup() {
+    // create an unique suite location peer test to avoid conflicts
     const tempRoot = fs.mkdtempSync(
       path.join(fs.realpathSync(os.tmpdir()), 'verdaccio-suite-test-')
     );
-    __global.addItem('dir-root', tempRoot);
+    debug('suite temporary folder %o', tempRoot);
+    __global.addItem('dir-suite-root', tempRoot);
+    // @ts-ignore
     this.global.__namespace = __global;
-    console.log(`current directory: ${process.cwd()}`);
+    debug(`current directory: ${process.cwd()}`);
   }
 
-  async teardown() {}
+  async teardown() {
+    // TODO: clean folder
+  }
 
   runScript(script): any {
     return super.runScript(script);
