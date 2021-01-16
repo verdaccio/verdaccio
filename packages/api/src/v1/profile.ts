2ea951d06ff29843250fc6cc80d117e9814c938f
Juan Picado <juanpicado19@gmail.com>
2021-01-16 10:40:43 +0100

    chore: update deps (#2056)
    * chore: update deps
    
    * chore: update eslint
    
    * chore: update deps types

--- packages/api/src/v1/profile.ts
+++ packages/api/src/v1/profile.ts
@@ -31,64 +31,64 @@ export default function (route: Router, auth: IAuth): void {
     };
   }
 
-  route.get('/-/npm/v1/user', function (
-    req: $RequestExtend,
-    res: Response,
-    next: $NextFunctionVer
-  ): void {
-    if (_.isNil(req.remote_user.name) === false) {
-      return next(buildProfile(req.remote_user.name));
-    }
+  route.get(
+    '/-/npm/v1/user',
+    function (req: $RequestExtend, res: Response, next: $NextFunctionVer): void {
+      if (_.isNil(req.remote_user.name) === false) {
+        return next(buildProfile(req.remote_user.name));
+      }
 
-    res.status(HTTP_STATUS.UNAUTHORIZED);
-    return next({
-      message: API_ERROR.MUST_BE_LOGGED,
-    });
-  });
-
-  route.post('/-/npm/v1/user', function (
-    req: $RequestExtend,
-    res: Response,
-    next: $NextFunctionVer
-  ): void {
-    if (_.isNil(req.remote_user.name)) {
       res.status(HTTP_STATUS.UNAUTHORIZED);
       return next({
         message: API_ERROR.MUST_BE_LOGGED,
       });
     }
+  );
 
-    const { password, tfa } = req.body;
-    const { name } = req.remote_user;
-
-    if (_.isNil(password) === false) {
-      if (validatePassword(password.new) === false) {
-        /* eslint new-cap:off */
-        return next(ErrorCode.getCode(HTTP_STATUS.UNAUTHORIZED, API_ERROR.PASSWORD_SHORT()));
-        /* eslint new-cap:off */
+  route.post(
+    '/-/npm/v1/user',
+    function (req: $RequestExtend, res: Response, next: $NextFunctionVer): void {
+      if (_.isNil(req.remote_user.name)) {
+        res.status(HTTP_STATUS.UNAUTHORIZED);
+        return next({
+          message: API_ERROR.MUST_BE_LOGGED,
+        });
       }
 
-      auth.changePassword(
-        name,
-        password.old,
-        password.new,
-        (err, isUpdated): $NextFunctionVer => {
-          if (_.isNull(err) === false) {
-            return next(
-              ErrorCode.getCode(err.status, err.message) || ErrorCode.getConflict(err.message)
-            );
-          }
+      const { password, tfa } = req.body;
+      const { name } = req.remote_user;
 
-          if (isUpdated) {
-            return next(buildProfile(req.remote_user.name));
-          }
-          return next(ErrorCode.getInternalError(API_ERROR.INTERNAL_SERVER_ERROR));
+      if (_.isNil(password) === false) {
+        if (validatePassword(password.new) === false) {
+          /* eslint new-cap:off */
+          return next(ErrorCode.getCode(HTTP_STATUS.UNAUTHORIZED, API_ERROR.PASSWORD_SHORT()));
+          /* eslint new-cap:off */
         }
-      );
-    } else if (_.isNil(tfa) === false) {
-      return next(ErrorCode.getCode(HTTP_STATUS.SERVICE_UNAVAILABLE, SUPPORT_ERRORS.TFA_DISABLED));
-    } else {
-      return next(ErrorCode.getCode(HTTP_STATUS.INTERNAL_ERROR, APP_ERROR.PROFILE_ERROR));
+
+        auth.changePassword(
+          name,
+          password.old,
+          password.new,
+          (err, isUpdated): $NextFunctionVer => {
+            if (_.isNull(err) === false) {
+              return next(
+                ErrorCode.getCode(err.status, err.message) || ErrorCode.getConflict(err.message)
+              );
+            }
+
+            if (isUpdated) {
+              return next(buildProfile(req.remote_user.name));
+            }
+            return next(ErrorCode.getInternalError(API_ERROR.INTERNAL_SERVER_ERROR));
+          }
+        );
+      } else if (_.isNil(tfa) === false) {
+        return next(
+          ErrorCode.getCode(HTTP_STATUS.SERVICE_UNAVAILABLE, SUPPORT_ERRORS.TFA_DISABLED)
+        );
+      } else {
+        return next(ErrorCode.getCode(HTTP_STATUS.INTERNAL_ERROR, APP_ERROR.PROFILE_ERROR));
+      }
     }
-  });
+  );
 }
