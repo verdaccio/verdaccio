711146b7a57827c3fd628247e371eddea31312e9
Juan Picado <juanpicado19@gmail.com>
2021-01-16 10:40:43 +0100

    chore: update deps (#2056)
    * chore: update deps
    
    * chore: update eslint
    
    * chore: update deps types

--- packages/web/src/endpoint/search.ts
+++ packages/web/src/endpoint/search.ts
@@ -8,48 +8,48 @@ import { $ResponseExtend, $RequestExtend, $NextFunctionVer } from './package';
 
 function addSearchWebApi(route: Router, storage: IStorageHandler, auth: IAuth): void {
   // Search package
-  route.get('/search/:anything', function (
-    req: $RequestExtend,
-    res: $ResponseExtend,
-    next: $NextFunctionVer
-  ): void {
-    const results: any = SearchInstance.query(req.params.anything);
-    // FUTURE: figure out here the correct type
-    const packages: any[] = [];
+  route.get(
+    '/search/:anything',
+    function (req: $RequestExtend, res: $ResponseExtend, next: $NextFunctionVer): void {
+      const results: any = SearchInstance.query(req.params.anything);
+      // FUTURE: figure out here the correct type
+      const packages: any[] = [];
 
-    const getPackageInfo = function (i): void {
-      storage.getPackage({
-        name: results[i].ref,
-        uplinksLook: false,
-        callback: (err, entry: Package): void => {
-          if (!err && entry) {
-            auth.allow_access({ packageName: entry.name }, req.remote_user, function (
-              err,
-              allowed
-            ): void {
-              if (err || !allowed) {
-                return;
-              }
+      const getPackageInfo = function (i): void {
+        storage.getPackage({
+          name: results[i].ref,
+          uplinksLook: false,
+          callback: (err, entry: Package): void => {
+            if (!err && entry) {
+              auth.allow_access(
+                { packageName: entry.name },
+                req.remote_user,
+                function (err, allowed): void {
+                  if (err || !allowed) {
+                    return;
+                  }
 
-              packages.push(entry.versions[entry[DIST_TAGS].latest]);
-            });
-          }
+                  packages.push(entry.versions[entry[DIST_TAGS].latest]);
+                }
+              );
+            }
 
-          if (i >= results.length - 1) {
-            next(packages);
-          } else {
-            getPackageInfo(i + 1);
-          }
-        },
-      });
-    };
+            if (i >= results.length - 1) {
+              next(packages);
+            } else {
+              getPackageInfo(i + 1);
+            }
+          },
+        });
+      };
 
-    if (results.length) {
-      getPackageInfo(0);
-    } else {
-      next([]);
+      if (results.length) {
+        getPackageInfo(0);
+      } else {
+        next([]);
+      }
     }
-  });
+  );
 }
 
 export default addSearchWebApi;
