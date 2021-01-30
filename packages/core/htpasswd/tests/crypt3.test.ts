96ca7f4949a2bb761301b6e0f81fbb8e06bd019c
Slava <slovaricheg@gmail.com>
2021-01-31 00:41:33 +0200

    feat: allow other password hashing algorithms (#1917) (#2072)

--- packages/core/htpasswd/tests/crypt3.test.ts
+++ packages/core/htpasswd/tests/crypt3.test.ts
@@ -1,10 +1,10 @@
-import { createSalt } from '../src/crypt3';
+import { createSalt, EncryptionMethod } from '../src/crypt3';
 
 jest.mock('crypto', () => {
   return {
-    randomBytes: (): { toString: () => string } => {
+    randomBytes: (len: number): { toString: () => string } => {
       return {
-        toString: (): string => '/UEGzD0RxSNDZA==',
+        toString: (): string => '/UEGzD0RxSNDZA=='.substring(0, len),
       };
     },
   };
@@ -12,20 +12,20 @@ jest.mock('crypto', () => {
 
 describe('createSalt', () => {
   test('should match with the correct salt type', () => {
-    expect(createSalt('crypt')).toEqual('/UEGzD0RxSNDZA==');
-    expect(createSalt('md5')).toEqual('$1$/UEGzD0RxSNDZA==');
-    expect(createSalt('blowfish')).toEqual('$2a$/UEGzD0RxSNDZA==');
-    expect(createSalt('sha256')).toEqual('$5$/UEGzD0RxSNDZA==');
-    expect(createSalt('sha512')).toEqual('$6$/UEGzD0RxSNDZA==');
+    expect(createSalt(EncryptionMethod.crypt)).toEqual('/U');
+    expect(createSalt(EncryptionMethod.md5)).toEqual('$1$/UEGzD0RxS');
+    expect(createSalt(EncryptionMethod.blowfish)).toEqual('$2a$/UEGzD0RxS');
+    expect(createSalt(EncryptionMethod.sha256)).toEqual('$5$/UEGzD0RxS');
+    expect(createSalt(EncryptionMethod.sha512)).toEqual('$6$/UEGzD0RxS');
   });
 
   test('should fails on unkwon type', () => {
     expect(function () {
-      createSalt('bad');
+      createSalt('bad' as any);
     }).toThrow(/Unknown salt type at crypt3.createSalt: bad/);
   });
 
   test('should generate legacy crypt salt by default', () => {
-    expect(createSalt()).toEqual(createSalt('crypt'));
+    expect(createSalt()).toEqual(createSalt(EncryptionMethod.crypt));
   });
 });
