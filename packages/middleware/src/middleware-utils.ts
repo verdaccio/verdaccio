d9f5d0b279b5695fa67efe198ae735630a094cf3
Diana Morales <dianmorales@gmail.com>
2021-02-03 11:27:17 +0100

    ref: Relocate utils functions to middleware pkg

---
+++ packages/middleware/src/middleware-utils.ts
@@ -0,0 +1,11 @@
+
+/**
+ * return package version from tarball name
+ * @param {String} name
+ * @returns {String}
+ */
+export function getVersionFromTarball(name: string): string | void {
+    // FIXME: we know the regex is valid, but we should improve this part as ts suggest
+    // @ts-ignore
+    return /.+-(\d.+)\.tgz/.test(name) ? name.match(/.+-(\d.+)\.tgz/)[1] : undefined;
+  }
