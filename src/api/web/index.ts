f88c61dd830c0e0acccae2925acb1442078ecc90
Juan Picado <juanpicado19@gmail.com>
2021-03-19 23:10:24 +0100

    chore: add host validation

--- src/api/web/index.ts
+++ src/api/web/index.ts
@@ -1,11 +1,15 @@
 import fs from 'fs';
 // import { URL } from 'url';
 import path from 'path';
+import { URL } from 'url';
 import LRU from 'lru-cache';
 import _ from 'lodash';
+import buildDebug from 'debug';
 import express from 'express';
+import isURLValidator from 'validator/lib/isURL';
+const debug = buildDebug('verdaccio');
 
-import { combineBaseUrl, getWebProtocol, isHTTPProtocol } from '../../lib/utils';
+import { getWebProtocol, isHTTPProtocol } from '../../lib/utils';
 import Search from '../../lib/search';
 import { HEADERS, HTTP_STATUS, WEB_TITLE } from '../../lib/constants';
 import loadPlugin from '../../lib/plugin-loader';
@@ -14,6 +18,7 @@ const { setSecurityWebHeaders } = require('../middleware');
 const pkgJSON = require('../../../package.json');
 
 const DEFAULT_LANGUAGE = 'es-US';
+const validProtocols = ['https', 'http'];
 
 export function loadTheme(config) {
   if (_.isNil(config.theme) === false) {
@@ -34,7 +39,8 @@ export function loadTheme(config) {
 export function validatePrimaryColor(primaryColor) {
   const isHex = /^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/i.test(primaryColor);
   if (!isHex) {
-    return '';
+    debug('invalid primary color %o', primaryColor);
+    return;
   }
 
   return primaryColor;
@@ -84,22 +90,21 @@ export default function (config, auth, storage) {
   });
 
   function renderHTML(req, res) {
-    const protocol = getWebProtocol(req.get(HEADERS.FORWARDED_PROTO), req.protocol);
-    const host = req.get('host');
-    const { url_prefix } = config;
-    const uri = `${protocol}://${host}`;
-    const base = combineBaseUrl(protocol, host, url_prefix);
+    const base = getPublicUrl(config?.url_prefix, req);
+    const basename = new URL(base).pathname;
     const language = config?.i18n?.web ?? DEFAULT_LANGUAGE;
     const darkMode = config?.web?.darkMode ?? false;
+    const title = config?.web?.title ?? WEB_TITLE;
+    const scope = config?.web?.scope ?? '';
     const primaryColor = validatePrimaryColor(config?.web?.primary_color);
-    const title = _.get(config, 'web.title') ? config.web.title : WEB_TITLE;
-    const scope = _.get(config, 'web.scope') ? config.web.scope : '';
     const options = {
-      uri,
       darkMode,
-      protocol,
-      host,
-      url_prefix,
+      // deprecated
+      protocol: '',
+      host: '',
+      uri: '',
+      // deprecated
+      url_prefix: basename,
       base,
       primaryColor,
       version: pkgJSON.version,
@@ -125,4 +130,81 @@ export default function (config, auth, storage) {
   });
 
   return router;
+}
+
+export function validateURL(publicUrl: string | void) {
+  try {
+    const parsed = new URL(publicUrl as string);
+    if (!validProtocols.includes(parsed.protocol.replace(':', ''))) {
+      throw Error('invalid protocol');
+    }
+    return true;
+  } catch (err) {
+    // TODO: add error logger here
+    return false;
+  }
+}
+
+export function isHost(url: string = '', options = {}): boolean {
+  return isURLValidator(url, {
+    require_host: true,
+    allow_trailing_dot: false,
+    require_valid_protocol: false,
+    // @ts-ignore
+    require_port: false,
+    require_tld: false,
+    ...options,
+  });
+}
+
+export function getPublicUrl(url_prefix: string = '', req): string {
+  if (validateURL(process.env.VERDACCIO_PUBLIC_URL as string)) {
+    const envURL = new URL(process.env.VERDACCIO_PUBLIC_URL as string).href;
+    debug('public url by env %o', envURL);
+    return envURL;
+  } else if (req.get('host')) {
+    const host = req.get('host');
+    if (!isHost(host)) {
+      throw new Error('invalid host');
+    }
+    const protocol = getWebProtocol(req.get(HEADERS.FORWARDED_PROTO), req.protocol);
+    const combinedUrl = combineBaseUrl(protocol, host, url_prefix);
+    debug('public url by request %o', combinedUrl);
+    return combinedUrl;
+  } else {
+    return '/';
+  }
+}
+
+/**
+ * Create base url for registry.
+ * @return {String} base registry url
+ */
+export function combineBaseUrl(protocol: string, host: string, prefix: string = ''): string {
+  debug('combined protocol %o', protocol);
+  debug('combined host %o', host);
+  const newPrefix = wrapPrefix(prefix);
+  debug('combined prefix %o', newPrefix);
+  const groupedURI = new URL(wrapPrefix(prefix), `${protocol}://${host}`);
+  const result = stripTrailingSlash(groupedURI.href);
+  debug('combined url %o', result);
+  return result;
+}
+
+const stripTrailingSlash = (str) => {
+  return str.endsWith('/') ? str.slice(0, -1) : str;
+};
+
+function wrapPrefix(prefix: string): string {
+  if (prefix === '' || _.isNil(prefix)) {
+    return '';
+  } else if (!prefix.startsWith('/') && prefix.endsWith('/')) {
+    return `/${prefix}`;
+  } else if (!prefix.startsWith('/') && !prefix.endsWith('/')) {
+    return `/${prefix}/`;
+  } else if (prefix.startsWith('/') && !prefix.endsWith('/')) {
+    return `${prefix}/`;
+  } else {
+    return prefix;
+  }
 }
