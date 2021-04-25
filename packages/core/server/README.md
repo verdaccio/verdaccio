cbdccf717760196f5401733070b52bc26b81c36f
Juan Picado <juanpicado19@gmail.com>
2021-04-25 13:17:50 +0200

    build: add fastify starting package (#2201)
    * chore: add fastify library as example with command
    
    * chore: update format
    
    * chore: add fastify package and start

---
+++ packages/core/server/README.md
@@ -0,0 +1,9 @@
+# Server Fastify (migration)
+
+A package intended to start a migration from Express to fastify. (WIP).
+
+Run from root folder
+
+```js
+pnpm debug -- new
+```
--- packages/verdaccio/debug/bootstrap.js
+++ packages/verdaccio/debug/bootstrap.js
@@ -1,5 +1,4 @@
 // this file aims to help local debugging with hot transpilation
-// it requires BABEL_ENV=registry set as env variable
 require('@babel/register')({
   extensions: ['.ts'],
 });
