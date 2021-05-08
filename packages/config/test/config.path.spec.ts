4e94350960972d8ee5685a564bf1b82d0b453729
Juan Picado <juanpicado19@gmail.com>
2021-05-08 23:48:31 +0200

    skip peer platform

--- packages/config/test/config.path.spec.ts
+++ packages/config/test/config.path.spec.ts
@@ -1,4 +1,5 @@
 import { findConfigFile } from '../src/config-path';
+import os from 'os';
 
 const mockmkDir = jest.fn();
 const mockaccessSync = jest.fn();
@@ -31,78 +32,74 @@ describe('config-path', () => {
   });
 
   describe('findConfigFile', () => {
-    describe('using defiled location from arguments', () => {
-      test('with custom location', () => {
-        expect(findConfigFile('/home/user/custom/location/config.yaml')).toEqual(
-          '/home/user/custom/location/config.yaml'
-        );
-        expect(mockwriteFile).not.toHaveBeenCalled();
-        expect(mockmkDir).not.toHaveBeenCalled();
-      });
-    });
-
-    describe('whith env variables', () => {
-      test('with XDG_CONFIG_HOME if directory exist but config file is missing', () => {
-        process.env.XDG_CONFIG_HOME = '/home/user';
-        expect(findConfigFile()).toEqual('/home/user/verdaccio/config.yaml');
-        expect(mockwriteFile).toHaveBeenCalledWith('/home/user/verdaccio/config.yaml');
-        expect(mockmkDir).toHaveBeenCalledWith('/home/user/verdaccio');
-      });
-
-      test('with HOME if directory exist but config file is missing', () => {
-        delete process.env.XDG_CONFIG_HOME;
-        process.env.HOME = '/home/user';
-        expect(findConfigFile()).toEqual('/home/user/.config/verdaccio/config.yaml');
-        expect(mockwriteFile).toHaveBeenCalledWith('/home/user/.config/verdaccio/config.yaml');
-        expect(mockmkDir).toHaveBeenCalledWith('/home/user/.config/verdaccio');
-      });
-
-      test('with windows as directory exist but config file is missing', () => {
-        const originalPlatform = process.platform;
-        Object.defineProperty(process, 'platform', {
-          value: 'win32',
+    if (os.platform() !== 'win32') {
+      describe('using defiled location from arguments', () => {
+        test('with custom location', () => {
+          expect(findConfigFile('/home/user/custom/location/config.yaml')).toEqual(
+            '/home/user/custom/location/config.yaml'
+          );
+          expect(mockwriteFile).not.toHaveBeenCalled();
+          expect(mockmkDir).not.toHaveBeenCalled();
         });
+      });
+
+      describe('whith env variables', () => {
+        test('with XDG_CONFIG_HOME if directory exist but config file is missing', () => {
+          process.env.XDG_CONFIG_HOME = '/home/user';
+          expect(findConfigFile()).toEqual('/home/user/verdaccio/config.yaml');
+          expect(mockwriteFile).toHaveBeenCalledWith('/home/user/verdaccio/config.yaml');
+          expect(mockmkDir).toHaveBeenCalledWith('/home/user/verdaccio');
+        });
+
+        test('with HOME if directory exist but config file is missing', () => {
+          delete process.env.XDG_CONFIG_HOME;
+          process.env.HOME = '/home/user';
+          expect(findConfigFile()).toEqual('/home/user/.config/verdaccio/config.yaml');
+          expect(mockwriteFile).toHaveBeenCalledWith('/home/user/.config/verdaccio/config.yaml');
+          expect(mockmkDir).toHaveBeenCalledWith('/home/user/.config/verdaccio');
+        });
+
+        describe('error handling', () => {
+          test('XDG_CONFIG_HOME is not directory fallback to default', () => {
+            process.env.XDG_CONFIG_HOME = '/home/user/fail';
+            mockaccessSync.mockImplementation(() => {});
+            mockwriteFile.mockImplementation(() => {});
+            expect(findConfigFile()).toMatch('packages/config/verdaccio/config.yaml');
+          });
+
+          test('no permissions on read default config file', () => {
+            process.env.XDG_CONFIG_HOME = '/home/user';
+            mockaccessSync.mockImplementation(() => {
+              throw new Error('error on write file');
+            });
+
+            expect(function () {
+              findConfigFile();
+            }).toThrow(/configuration file does not have enough permissions for reading/);
+          });
+        });
+      });
+
+      describe('with no env variables', () => {
+        test('with relative location', () => {
+          mockaccessSync.mockImplementation(() => {});
+          delete process.env.XDG_CONFIG_HOME;
+          delete process.env.HOME;
+          process.env.APPDATA = '/app/data/';
+          expect(findConfigFile()).toMatch('packages/config/verdaccio/config.yaml');
+          expect(mockwriteFile).toHaveBeenCalled();
+          expect(mockmkDir).toHaveBeenCalled();
+        });
+      });
+    } else {
+      test('with windows as directory exist but config file is missing', () => {
         delete process.env.XDG_CONFIG_HOME;
         delete process.env.HOME;
         process.env.APPDATA = '/app/data/';
         expect(findConfigFile()).toEqual('/app/data/verdaccio/config.yaml');
         expect(mockwriteFile).toHaveBeenCalledWith('/app/data/verdaccio/config.yaml');
         expect(mockmkDir).toHaveBeenCalledWith('/app/data/verdaccio');
-        Object.defineProperty(process, 'platform', {
-          value: originalPlatform,
-        });
       });
-      describe('error handling', () => {
-        test('XDG_CONFIG_HOME is not directory fallback to default', () => {
-          process.env.XDG_CONFIG_HOME = '/home/user/fail';
-          mockaccessSync.mockImplementation(() => {});
-          mockwriteFile.mockImplementation(() => {});
-          expect(findConfigFile()).toMatch('packages/config/verdaccio/config.yaml');
-        });
-
-        test('no permissions on read default config file', () => {
-          process.env.XDG_CONFIG_HOME = '/home/user';
-          mockaccessSync.mockImplementation(() => {
-            throw new Error('error on write file');
-          });
-
-          expect(function () {
-            findConfigFile();
-          }).toThrow(/configuration file does not have enough permissions for reading/);
-        });
-      });
-    });
-
-    describe('with no env variables', () => {
-      test('with relative location', () => {
-        mockaccessSync.mockImplementation(() => {});
-        delete process.env.XDG_CONFIG_HOME;
-        delete process.env.HOME;
-        process.env.APPDATA = '/app/data/';
-        expect(findConfigFile()).toMatch('packages/config/verdaccio/config.yaml');
-        expect(mockwriteFile).toHaveBeenCalled();
-        expect(mockmkDir).toHaveBeenCalled();
-      });
-    });
+    }
   });
 });
