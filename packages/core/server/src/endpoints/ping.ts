29aadc76c702f5a328eb84a348b2c2468b888e5e
Juan Picado <juanpicado19@gmail.com>
2021-05-13 00:31:36 +0200

    chore: format

--- packages/core/server/src/endpoints/ping.ts
+++ packages/core/server/src/endpoints/ping.ts
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin';
 
 import { logger } from '@verdaccio/logger';
 
-function pingPlugin (fastify, _opts, done) {
+function pingPlugin(fastify, _opts, done) {
   fastify.get('/-/ping', async () => {
     logger.http('ping endpoint');
     return {};
@@ -13,5 +13,5 @@ function pingPlugin (fastify, _opts, done) {
 
 export default fp(pingPlugin, {
   fastify: '3.x',
-  name: 'api-ping'
+  name: 'api-ping',
 });
