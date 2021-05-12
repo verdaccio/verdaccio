a298a1783f663600eb3b3a619fcb84e9bcb91435
Juan Picado <juanpicado19@gmail.com>
2021-05-13 00:31:06 +0200

    endpoint as plugin

--- packages/core/server/src/endpoints/ping.ts
+++ packages/core/server/src/endpoints/ping.ts
@@ -1,10 +1,17 @@
+import fp from 'fastify-plugin';
+
 import { logger } from '@verdaccio/logger';
 
-async function pingRoute(fastify) {
+function pingPlugin (fastify, _opts, done) {
   fastify.get('/-/ping', async () => {
     logger.http('ping endpoint');
     return {};
   });
+
+  done();
 }
 
-export default pingRoute;
+export default fp(pingPlugin, {
+  fastify: '3.x',
+  name: 'api-ping'
+});
