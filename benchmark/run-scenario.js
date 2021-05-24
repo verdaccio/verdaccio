89da4fcceb6368b5bc45a38c2334c3f52530b46d
Juan Picado <juanpicado19@gmail.com>
2021-05-24 17:44:13 +0200

    script

--- benchmark/run-scenario.js
+++ benchmark/run-scenario.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 
 (async () => {
   // console.log('1');
-  // const response = await fetch('http://localhost:8000/aaa');
+  // const response = await fetch('http://localhost:4873/aaa');
   // const r = await response.text();
   // console.log('---', r);
   setTimeout(function () {
