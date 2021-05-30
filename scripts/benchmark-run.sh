af678755f86a048082597b4899bed336235f0336
Juan Picado <juanpicado19@gmail.com>
2021-05-30 21:19:01 +0200

    test

--- scripts/benchmark-run.sh
+++ scripts/benchmark-run.sh
@@ -5,4 +5,16 @@ set -e
 VERSION=$1
 FIXTURE=$2
 
-hyperfine --ignore-failure --warmup 1 --runs 2 --show-output --export-json ./hyper-results.json--prepare ./scripts/benchmark-prepare.sh $2
+case $FIXTURE in 
+  info)
+    FIXTURE='npm info jquery'
+    ;;
+  tarball)
+    FIXTURE='npm install jquery'
+    ;;
+  *)
+    echo "no command found"  
+    return 1;;
+esac
+
+hyperfine --ignore-failure --warmubp 1 --runs 2 --show-output --export-json './hyper-results.json' --prepare ./scripts/benchmark-prepare.sh $FIXTURE
