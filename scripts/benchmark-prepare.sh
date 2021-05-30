bfcc9735cbe5d960435fb795fdbe53bf6d40837e
Juan Picado <juanpicado19@gmail.com>
2021-05-30 20:27:14 +0200

    test

--- scripts/benchmark-prepare.sh
+++ scripts/benchmark-prepare.sh
@@ -6,6 +6,7 @@ VERSION=$1
 
 tmp_registry_log=`mktemp`
 
-nohup npx verdaccio@$VERSION &>$tmp_registry_log &
+npm i -g verdaccio@$VERSION
+nohup verdaccio &>$tmp_registry_log &
 grep -q 'http address' <(tail -f $tmp_registry_log)
 npm set registry http://localhost:4873
