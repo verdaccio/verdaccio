67406082ed53fb630547fa7229b540f476f636e9
Juan Picado <juanpicado19@gmail.com>
2021-06-12 21:51:54 +0200

    update to react17 and other dependencies on ui (#2295)

---
+++ .changeset/few-mangos-grow.md
@@ -0,0 +1,5 @@
+---
+'@verdaccio/ui-theme': minor
+---
+
+upgrade to react@17 and other dependencies
--- packages/plugins/ui-theme/src/components/Avatar.tsx
+++ packages/plugins/ui-theme/src/components/Avatar.tsx
@@ -1,11 +1,3 @@
-import { default as MaterialUIAvatar, AvatarProps } from '@material-ui/core/Avatar';
-import React, { forwardRef } from 'react';
+import { default as MaterialUIAvatar } from '@material-ui/core/Avatar';
 
-// The default element type of MUI's Avatar is 'div' and we don't allow the change of this prop
-type AvatarRef = HTMLDivElement;
-
-const Avatar = forwardRef<AvatarRef, AvatarProps>(function Avatar(props, ref) {
-  return <MaterialUIAvatar {...props} ref={ref} />;
-});
-
-export default Avatar;
+export default MaterialUIAvatar;
