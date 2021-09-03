7bba4d0f0caf1415294dfdf0ca929e785489b367
Juan Picado <juanpicado19@gmail.com>
2021-09-03 22:21:57 +0200

    node 14 as minimum for cli

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,6 +1,6 @@
 import semver from 'semver';
 
-export const MIN_NODE_VERSION = '12';
+export const MIN_NODE_VERSION = '14';
 
 export function isVersionValid(version) {
   return semver.satisfies(version, `>=${MIN_NODE_VERSION}`);
