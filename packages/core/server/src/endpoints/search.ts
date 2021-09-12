e50410a87570bf92c193bb49a82215ea7e655116
Juan Picado <juanpicado19@gmail.com>
2021-09-12 17:12:47 +0200

    feat: fastify search endpoint v1 without auth and format (#2432)

--- packages/core/server/src/endpoints/search.ts
+++ packages/core/server/src/endpoints/search.ts
@@ -3,12 +3,22 @@
 import { logger } from '@verdaccio/logger';
 
 async function searchRoute(fastify) {
-  fastify.get('/-/v1/search', async () => {
+  fastify.get('/-/v1/search', async (request, reply) => {
+    // TODO: apply security layer here like in
+    // packages/api/src/v1/search.ts
+    // TODO: add validations for query, some parameters are mandatory
+    // TODO: review which query fields are mandatory
+
+    const { url, query } = request;
+    const storage = fastify.storage;
+
+    const data = await storage.searchManager?.search({
+      query: query,
+      url: url,
+    });
+
     logger.http('search endpoint');
-    // @ts-ignore
-    console.log('-storage->', fastify.storage);
-    console.log('-config->', fastify.config);
-    return {};
+    reply.code(200).send(data);
   });
 }
 
