2c594910d85efb22e7dbea5b8b2ba232da036477
Zameer Haque <20766601+haquezameer@users.noreply.github.com>
2021-09-19 23:50:46 +0530

    fix: do not throw multiple logger deprecation warning if using default logger config (#2446)

--- packages/logger/test/logger.spec.ts
+++ packages/logger/test/logger.spec.ts
@@ -13,4 +13,29 @@ describe('logger', () => {
     // FIXME: check expect
     // expect(spyOn).toHaveBeenCalledTimes(2);
   });
+
+  test('throw deprecation warning if multiple loggers configured', () => {
+    const spy = jest.spyOn(process, 'emitWarning');
+    setup([
+      {
+        level: 'info',
+      },
+      {
+        level: 'http',
+      },
+    ]);
+    expect(spy).toHaveBeenCalledWith(
+      'deprecate: multiple logger configuration is deprecated, please check the migration guide.'
+    );
+    spy.mockRestore();
+  });
+
+  test('regression: do not throw deprecation warning if no logger config is provided', () => {
+    const spy = jest.spyOn(process, 'emitWarning');
+    setup();
+    expect(spy).not.toHaveBeenCalledWith(
+      'deprecate: multiple logger configuration is deprecated, please check the migration guide.'
+    );
+    spy.mockRestore();
+  });
 });
