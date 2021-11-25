549eb07bdf1e51a8def34ea26497bde94e6c109e
Juan Picado <juanpicado19@gmail.com>
2021-11-25 23:01:54 +0100

    fix lint

--- website/src/components/Contributors.tsx
+++ website/src/components/Contributors.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import Layout from '@theme/Layout';
+import React from 'react';
 
 type ContributorsProps = {
   contributors: any;
 };
 
-const Contributors = ({ contributors }): React.ReactElement => (
+const Contributors: React.FC<ContributorsProps> = ({ contributors }): React.ReactElement => (
   <Layout
     title="Contributors 2"
     description="Verdaccio Contributors, thanks to the community Verdaccio keeps running">
-      <div>contributors: {JSON.stringify(contributors, null, 3)}</div>
+    <div>contributors: {JSON.stringify(contributors, null, 3)}</div>
   </Layout>
 );
 
