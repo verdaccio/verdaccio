23760c536776d4a30a63cc43406fc34236669173
Juan Picado <juanpicado19@gmail.com>
2021-12-24 13:45:21 +0100

    chore: fix params

--- src/api/web/endpoint/index.ts
+++ src/api/web/endpoint/index.ts
@@ -7,8 +7,8 @@ import user from './user';
 export default (auth, storage, config) => {
   const route = Router(); /* eslint new-cap: 0 */
   route.use('/data/', limiter(config?.web?.rateLimit));
-  route.use('/data/', packageApi(auth, storage, config));
-  route.use('/data/', search(auth, storage));
+  route.use('/data/', packageApi(storage, auth, config));
+  route.use('/data/', search(storage, auth));
   route.use('/sec/', limiter(config?.userRateLimit));
   route.use('/sec/', user(auth, storage));
   return route;
