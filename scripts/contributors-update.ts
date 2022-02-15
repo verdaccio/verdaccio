cc30a85d3d77b8c72a57abe1c67cc34ea071ce12
Juan Picado <juanpicado19@gmail.com>
2022-02-15 22:18:20 +0100

    chore: script to update contributors (#3001)
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors
    
    * chore: script to update contributors

---
+++ scripts/contributors-update.ts
@@ -0,0 +1,40 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const token = process.env.TOKEN;
+// TODO: migrate to ESM and import
+const contributors = require('@dianmora/contributors');
+const excludebots = [
+  'verdacciobot',
+  'dependabot-preview[bot]',
+  'dependabot[bot]',
+  '64b2b6d12bfe4baae7dad3d01',
+  'greenkeeper[bot]',
+  'snyk-bot',
+  'allcontributors[bot]',
+  'renovate[bot]',
+  'undefined',
+  'renovate-bot',
+];
+
+(async () => {
+  try {
+    // Awesome script made by https://github.com/dianmorales
+    const result = await contributors({
+      token: token,
+      organization: 'verdaccio',
+      excludebots,
+      allowFork: false,
+      allowPrivateRepo: false,
+    });
+    const pathContributorsFile = path.join(
+      __dirname,
+      '../packages/tools/docusaurus-plugin-contributors/src/contributors.json'
+    );
+    await fs.writeFile(pathContributorsFile, JSON.stringify(result, null, 4));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('error on update', err);
+    process.exit(1);
+  }
+})();
