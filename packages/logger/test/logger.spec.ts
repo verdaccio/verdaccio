bc1052a37a8bf4b65d01e189a4516cd44b09b5d6
Juan Picado <juanpicado19@gmail.com>
2022-02-19 14:43:30 +0100

    migrate logger plugin to esm

--- packages/logger/test/logger.spec.ts
+++ packages/logger/test/logger.spec.ts
@@ -1,40 +1,10 @@
-import { warningUtils } from '@verdaccio/core';
+import { describe, expect, test } from 'vitest';
 
-import { logger, setup } from '../src';
-
-const mockWarningUtils = jest.fn();
-
-jest.mock('@verdaccio/core', () => {
-  const original = jest.requireActual('@verdaccio/core');
-  return {
-    warningUtils: {
-      ...original.warningUtils,
-      emit: (...args) => {
-        mockWarningUtils(...args);
-      },
-    },
-  };
-});
+import { setup } from '../src';
 
 describe('logger', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test.skip('should write message logger', () => {
-    jest.spyOn(process.stdout, 'write');
-    setup([
-      {
-        level: 'info',
-      },
-    ]);
-
-    logger.info({ packageName: 'test' }, `publishing or updating a new version for @{packageName}`);
-    // FIXME: check expect
-    // expect(spyOn).toHaveBeenCalledTimes(2);
-  });
-
-  test('throw deprecation warning if multiple loggers configured', () => {
+  // this must be first due the warning code throws first the desired warning
+  test('throw deprecation warning if multiple loggers configured', (done) => {
     setup([
       {
         level: 'info',
@@ -43,11 +13,14 @@ describe('logger', () => {
         level: 'http',
       },
     ]);
-    expect(mockWarningUtils).toHaveBeenCalledWith(warningUtils.Codes.VERDEP002);
+    process.once('warning', function (warning: { code: string }) {
+      expect(warning.code).toEqual('VERDEP002');
+      done();
+    });
   });
 
-  test('regression: do not throw deprecation warning if no logger config is provided', () => {
+  test('should instantiace well', () => {
     setup();
-    expect(mockWarningUtils).not.toHaveBeenCalled();
+    expect(() => setup()).not.toThrowError();
   });
 });
