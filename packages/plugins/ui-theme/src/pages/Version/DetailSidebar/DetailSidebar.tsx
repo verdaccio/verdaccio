aa0b2aa9df45edbfb26888468e805be906286b17
Juan Picado <juanpicado19@gmail.com>
2022-03-03 00:05:36 +0100

    fix: replace ts icon by td and fix commonjs icon

--- packages/plugins/ui-theme/src/pages/Version/DetailSidebar/DetailSidebar.tsx
+++ packages/plugins/ui-theme/src/pages/Version/DetailSidebar/DetailSidebar.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import React, { useContext } from 'react';
+import { ModuleType, PackageMetaInterface } from 'types/packageMeta';
 import ActionBar from 'verdaccio-ui/components/ActionBar';
 import Author from 'verdaccio-ui/components/Author';
 import Paper from 'verdaccio-ui/components/Paper';
@@ -17,6 +18,15 @@ const Dist = loadable(() => import(/* webpackChunkName: "Dist" */ './Dist'));
 const Install = loadable(() => import(/* webpackChunkName: "Install" */ './Install'));
 const Repository = loadable(() => import(/* webpackChunkName: "Repository" */ './Repository'));
 
+const getModuleType = (manifest: PackageMetaInterface) => {
+  if (manifest.latest.main) {
+    return 'commonjs';
+  } else if (manifest.latest.type) {
+    return manifest.latest.type;
+  }
+  return;
+};
+
 const DetailSidebar: React.FC = () => {
   const detailContext = useContext(DetailContext);
   const { packageMeta, packageName, packageVersion } = detailContext;
@@ -31,7 +41,7 @@ const DetailSidebar: React.FC = () => {
         description={packageMeta.latest?.description}
         hasTypes={typeof packageMeta.latest.types === 'string'}
         isLatest={typeof packageVersion === 'undefined'}
-        moduleType={packageMeta.latest.type}
+        moduleType={getModuleType(packageMeta)}
         packageName={packageName}
         version={packageVersion || packageMeta.latest.version}
       />
