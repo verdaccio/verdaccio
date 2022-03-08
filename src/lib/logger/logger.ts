0f51a6ced5ee453b1cb6d7b375960d4b21ea4834
Juan Picado <juanpicado19@gmail.com>
2022-03-08 18:30:29 +0100

    fix: missing colors on terminal (#3049)

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -111,7 +111,7 @@ export function setup(options: LoggerConfig | LoggerConfigItem = [DEFAULT_LOGGER
     );
   }
   const pinoConfig = { level: loggerConfig.level };
-  let colors = loggerConfig?.colors === true ? process.stdout.isTTY : false;
+  let colors = typeof loggerConfig?.colors === 'boolean' ? Boolean(loggerConfig?.colors) : process.stdout.isTTY;
   if ('EXPERIMENTAL_VERDACCIO_LOGGER_COLORS' in process.env) {
     colors = process.env.EXPERIMENTAL_VERDACCIO_LOGGER_COLORS != 'false';
   }
