b0fa890c0770164af70d0f1151fc919256fe373e
Juan Picado <juanpicado19@gmail.com>
2022-08-18 20:22:24 +0200

    chore: set time fix timezone for tests

--- packages/logger-prettify/test/utils.test.ts
+++ packages/logger-prettify/test/utils.test.ts
@@ -1,12 +1,8 @@
-import MockDate from 'mockdate';
-
 import { formatLoggingDate, padLeft, padRight } from '../src/utils';
 
 describe('formatLoggingDate', () => {
   test('basic', () => {
-    const mockDate = '2018-01-14T11:17:40.712Z';
-    MockDate.set(mockDate);
-    expect(formatLoggingDate(Date.now(), ' message')).toEqual('[2018-01-14 12:17:40] message');
+    expect(formatLoggingDate(1585411248203, ' message')).toEqual('[2020-03-28 16:00:48] message');
   });
 });
 
