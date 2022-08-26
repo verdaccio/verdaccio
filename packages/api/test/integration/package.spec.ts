b820c256d41ceac28e4724a9fdc3eee8fd0c3a0b
Juan Picado <juanpicado19@gmail.com>
2022-08-26 21:40:33 +0200

    chore: add tests

--- packages/api/test/integration/package.spec.ts
+++ packages/api/test/integration/package.spec.ts
@@ -1,6 +1,7 @@
 import supertest from 'supertest';
 
-import { HEADERS, HEADER_TYPE, HTTP_STATUS } from '@verdaccio/core';
+import { DIST_TAGS, HEADERS, HEADER_TYPE, HTTP_STATUS } from '@verdaccio/core';
+import { Storage } from '@verdaccio/store';
 
 import { initializeServer, publishVersion } from './_helper';
 
@@ -43,6 +44,26 @@ describe('package', () => {
         .expect(HEADER_TYPE.CONTENT_TYPE, HEADERS.JSON_CHARSET)
         .expect(HTTP_STATUS.OK);
       expect(response.body.name).toEqual(pkg);
+    }
+  );
+
+  test.each([['foo-abbreviated'], ['@scope/foo-abbreviated']])(
+    'should return abbreviated local manifest',
+    async (pkg) => {
+      await publishVersion(app, pkg, '1.0.0');
+      const response = await supertest(app)
+        .get(`/${pkg}`)
+        .set(HEADERS.ACCEPT, HEADERS.JSON)
+        .set(HEADERS.ACCEPT, Storage.ABBREVIATED_HEADER)
+        .expect(HEADER_TYPE.CONTENT_TYPE, HEADERS.JSON_CHARSET)
+        .expect(HTTP_STATUS.OK);
+      expect(response.body.name).toEqual(pkg);
+      expect(response.body.time).toBeDefined();
+      expect(response.body.modified).toBeDefined();
+      expect(response.body[DIST_TAGS]).toEqual({ latest: '1.0.0' });
+      expect(response.body.readme).not.toBeDefined();
+      expect(response.body._rev).not.toBeDefined();
+      expect(response.body.users).not.toBeDefined();
     }
   );
 });
