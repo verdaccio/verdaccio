ed7f06a0022d8b660b4ecdb2a1d0a3ffc1473eff
Juan Picado <juanpicado19@gmail.com>
2022-09-16 22:22:41 +0200

    Update process.ts

--- test/cli/cli-commons/src/process.ts
+++ test/cli/cli-commons/src/process.ts
@@ -41,7 +41,7 @@ export async function exec(options: SpawnOptions, cmd, args): Promise<ExecOutput
   const err = new Error(`Running "${cmd} ${args.join(' ')}" returned error code `);
   return new Promise((resolve, reject) => {
     childProcess.on('exit', (error) => {
-      if (!error && typeof stderr !== 'undefined') {
+      if (!error) {
         resolve({ stdout, stderr });
       } else {
         err.message += `${error}...\n\nSTDOUT:\n${stdout}\n\nSTDERR:\n${stderr}\n`;
