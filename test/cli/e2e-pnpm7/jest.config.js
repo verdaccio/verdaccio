9c987103d19f0bd95ccb59caf1668af638de4655
Juan Picado <juanpicado19@gmail.com>
2022-09-18 18:14:54 +0200

    chore: improve setup

--- test/cli/e2e-pnpm7/jest.config.js
+++ test/cli/e2e-pnpm7/jest.config.js
@@ -2,5 +2,6 @@ const { defaults } = require('jest-config');
 const config = require('../../../jest/config');
 
 module.exports = Object.assign({}, config, {
+  collectCoverage: false,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts'],
 });
--- test/cli/e2e-yarn1/jest.config.js
+++ test/cli/e2e-yarn1/jest.config.js
@@ -2,13 +2,6 @@ const { defaults } = require('jest-config');
 const config = require('../../../jest/config');
 
 module.exports = Object.assign({}, config, {
-  name: 'verdaccio-e2e-cli-jest',
-  // verbose: true,
-  // collectCoverage: false,
+  collectCoverage: false,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts'],
-  // testEnvironment: './env_babel.js',
-  // globalSetup: './env_setup.js',
-  // globalTeardown: './env_teardown.js',
-  // testRegex: '(/test/e2e.*\\.spec)\\.ts',
-  testRegex: '(/test/*.*.spec)\\.ts',
 });
