18348940c4989a04ee22c5418cf6a1e7d11b882d
Juan Picado <juanpicado19@gmail.com>
2022-09-19 23:10:38 +0200

    e2e: npm search and ping commands (#3386)
    * chore: rename folders
    
    * chore: add search tests
    
    * chore: remove old files
    
    * Update pnpm-workspace.yaml
    
    * chore: fix ui
    
    * Update e2e-ci.yml
    
    * chore: renovate deps
    
    * fix build
    
    * chore: add tests

--- e2e/ui/jest.config.js
+++ e2e/ui/jest.config.js
@@ -1,10 +1,8 @@
 const config = require('../../jest/config');
 
 module.exports = Object.assign({}, config, {
-  name: 'verdaccio-e2e-jest',
   verbose: false,
   collectCoverage: false,
   globalSetup: './pre-setup.js',
   globalTeardown: './teardown.js',
-  testEnvironment: './puppeteer_environment.js',
 });
