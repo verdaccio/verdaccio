8306fa895ed67ed299a4f91de08c1031c3026f7f
Juan Picado <juanpicado19@gmail.com>
2022-09-24 08:07:59 +0200

    chore: fix export

--- e2e/cli/e2e-pnpm7/utils.ts
+++ e2e/cli/e2e-pnpm7/utils.ts
@@ -4,19 +4,19 @@ import { join } from 'path';
 import { exec } from '@verdaccio/test-cli-commons';
 import { addRegistry } from '@verdaccio/test-cli-commons';
 
-export function getCommand() {
+function getCommand() {
   return join(__dirname, './node_modules/.bin/pnpm');
 }
 
-export function pnpm(options: SpawnOptions, ...args: string[]) {
+function pnpm(options: SpawnOptions, ...args: string[]) {
   return exec(options, getCommand(), args);
 }
 
-export async function bumbUp(tempFolder, registry) {
+async function bumbUp(tempFolder, registry) {
   await pnpm({ cwd: tempFolder }, 'version', 'minor', ...addRegistry(registry.getRegistryUrl()));
 }
 
-export async function publish(tempFolder, pkgName, registry, arg: string[] = []) {
+async function publish(tempFolder, pkgName, registry, arg: string[] = []) {
   const resp = await pnpm(
     { cwd: tempFolder },
     'publish',
@@ -28,7 +28,7 @@ export async function publish(tempFolder, pkgName, registry, arg: string[] = [])
   expect(parsedBody.name).toEqual(pkgName);
 }
 
-export async function getInfoVersions(pkgName, registry) {
+async function getInfoVersions(pkgName, registry) {
   const infoResp = await pnpm(
     {},
     'info',
@@ -39,3 +39,5 @@ export async function getInfoVersions(pkgName, registry) {
   const infoBody = JSON.parse(infoResp.stdout as string);
   return infoBody;
 }
+
+export { getInfoVersions, pnpm, publish, bumbUp };
