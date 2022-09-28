d4019f6349dcc17e4a01160b0244d36eb5502588
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-09-28 01:26:18 -0400

    feat: improve "Current Tags" version list (#3401)

---
+++ .changeset/sour-maps-live.md
@@ -0,0 +1,5 @@
+---
+'@verdaccio/ui-theme': patch
+---
+
+Add links to "Current Tags" and sort them in descending order
--- packages/plugins/ui-theme/src/pages/Version/DetailContainer/Versions/VersionsTagList.tsx
+++ packages/plugins/ui-theme/src/pages/Version/DetailContainer/Versions/VersionsTagList.tsx
@@ -3,19 +3,26 @@ import ListItem from '@mui/material/ListItem';
 import React from 'react';
 
 import { DistTags } from '../../../../../types/packageMeta';
-import { ListItemText, Spacer } from './styles';
+import { Time } from '../../../../../types/packageMeta';
+import { ListItemText, Spacer, StyledLink } from './styles';
 
 interface Props {
   tags: DistTags;
+  packageName: string;
+  time: Time;
 }
 
-const VersionsTagList: React.FC<Props> = ({ tags }) => (
+const VersionsTagList: React.FC<Props> = ({ tags, packageName, time }) => (
   <List dense={true}>
     {Object.keys(tags)
-      .reverse()
+      .sort((a, b) => {
+        return time[tags[a]] < time[tags[b]] ? 1 : time[tags[a]] > time[tags[b]] ? -1 : 0;
+      })
       .map((tag) => (
         <ListItem className="version-item" key={tag}>
-          <ListItemText>{tag}</ListItemText>
+          <StyledLink to={`/-/web/detail/${packageName}/v/${tags[tag]}`}>
+            <ListItemText>{tag}</ListItemText>
+          </StyledLink>
           <Spacer />
           <ListItemText>{tags[tag]}</ListItemText>
         </ListItem>
