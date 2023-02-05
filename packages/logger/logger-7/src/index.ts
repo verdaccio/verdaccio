6e0e8cf8c8d6900d187e902348b57837aaad467c
Juan Picado <juanpicado19@gmail.com>
2023-02-05 20:38:25 +0100

    Update index.ts

--- packages/logger/logger-7/src/index.ts
+++ packages/logger/logger-7/src/index.ts
@@ -1,9 +1,18 @@
 import pino from 'pino';
 
 import { prepareSetup } from '@verdaccio/logger-commons';
-import { Logger, LoggerConfigItem } from '@verdaccio/types';
+import { Logger, LoggerFormat, LoggerType } from '@verdaccio/types';
 
 let logger: Logger;
+
+export type LoggerConfigItem = {
+  type?: LoggerType;
+  format?: LoggerFormat;
+  path?: string;
+  level?: string;
+  colors?: boolean;
+  async?: boolean;
+};
 
 export function setup(options: LoggerConfigItem) {
   if (typeof logger !== 'undefined') {
