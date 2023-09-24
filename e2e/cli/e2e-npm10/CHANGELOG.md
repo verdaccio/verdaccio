2b5a15c22b5857d88ce7ca1d9d0a5cdf7d8c491b
Juan Picado <juanpicado19@gmail.com>
2023-09-24 12:44:39 +0200

    chore: add e2e npm 10 e2e (#4036)

---
+++ e2e/cli/e2e-npm10/CHANGELOG.md
@@ -0,0 +1,62 @@
+# @verdaccio/e2e-cli-npm9
+
+## 1.0.1
+
+### Patch Changes
+
+- Updated dependencies [351aeeaa8]
+- Updated dependencies [d167f92e1]
+- Updated dependencies [c383eb68c]
+  - @verdaccio/test-cli-commons@1.1.0
+
+## 1.0.1-6-next.7
+
+### Patch Changes
+
+- Updated dependencies [c383eb68]
+  - @verdaccio/test-cli-commons@1.1.0-6-next.7
+
+## 1.0.1-6-next.6
+
+### Patch Changes
+
+- Updated dependencies [d167f92e]
+  - @verdaccio/test-cli-commons@1.1.0-6-next.6
+
+## 1.0.1-6-next.5
+
+### Patch Changes
+
+- @verdaccio/test-cli-commons@1.0.1-6-next.5
+
+## 1.0.1-6-next.4
+
+### Patch Changes
+
+- @verdaccio/test-cli-commons@1.0.1-6-next.4
+
+## 1.0.1-6-next.3
+
+### Patch Changes
+
+- 351aeeaa: fix(deps): @verdaccio/utils should be a prod dep of local-storage
+- Updated dependencies [351aeeaa]
+  - @verdaccio/test-cli-commons@1.0.1-6-next.3
+
+## 1.0.1-6-next.2
+
+### Patch Changes
+
+- @verdaccio/test-cli-commons@1.0.1-6-next.2
+
+## 1.0.1-6-next.1
+
+### Patch Changes
+
+- @verdaccio/test-cli-commons@1.0.1-6-next.1
+
+## 1.0.1-6-next.0
+
+### Patch Changes
+
+- @verdaccio/test-cli-commons@1.0.1-6-next.0
---
+++ e2e/cli/e2e-npm10/utils.ts
@@ -0,0 +1,41 @@
+import { SpawnOptions } from 'child_process';
+import { join } from 'path';
+
+import { exec } from '@verdaccio/test-cli-commons';
+import { addRegistry } from '@verdaccio/test-cli-commons';
+
+export function getCommand() {
+  return join(__dirname, './node_modules/.bin/npm');
+}
+
+export function npm(options: SpawnOptions, ...args: string[]) {
+  return exec(options, getCommand(), args);
+}
+
+export async function bumbUp(tempFolder, registry) {
+  await npm({ cwd: tempFolder }, 'version', 'minor', ...addRegistry(registry.getRegistryUrl()));
+}
+
+export async function publish(tempFolder, pkgName, registry, arg: string[] = []) {
+  const resp = await npm(
+    { cwd: tempFolder },
+    'publish',
+    ...arg,
+    '--json',
+    ...addRegistry(registry.getRegistryUrl())
+  );
+  const parsedBody = JSON.parse(resp.stdout as string);
+  expect(parsedBody.name).toEqual(pkgName);
+}
+
+export async function getInfoVersions(pkgName, registry) {
+  const infoResp = await npm(
+    {},
+    'info',
+    pkgName,
+    '--json',
+    ...addRegistry(registry.getRegistryUrl())
+  );
+  const infoBody = JSON.parse(infoResp.stdout as string);
+  return infoBody;
+}
