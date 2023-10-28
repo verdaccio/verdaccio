8bb974c4f68e583dae5b88cc2ba29463e8bb8d49
Juan Picado <juanpicado19@gmail.com>
2023-10-28 17:51:02 +0200

    chore: upgrade yarn 4 (#4094)
    * chore: upgrade yarn 4
    
    * Update user.ts

--- src/api/web/api/user.ts
+++ src/api/web/api/user.ts
@@ -2,10 +2,9 @@ import { Request, Response, Router } from 'express';
 import _ from 'lodash';
 
 import { rateLimit } from '@verdaccio/middleware';
+import { SignOptionsSignature } from '@verdaccio/signature';
 import { Config, RemoteUser } from '@verdaccio/types';
-import {
-  SignOptionsSignature
-} from '@verdaccio/signature';
+
 import Auth from '../../../lib/auth';
 import { getSecurity, validatePassword } from '../../../lib/auth-utils';
 import { API_ERROR, APP_ERROR, HEADERS, HTTP_STATUS } from '../../../lib/constants';
@@ -25,7 +24,8 @@ function addUserAuthApi(route: Router, auth: Auth, config: Config): Router {
           next(ErrorCode.getCode(errorCode, err.message));
         } else {
           req.remote_user = user;
-          const jWTSignOptions: SignOptionsSignature = getSecurity(config).web.sign as SignOptionsSignature;
+          const jWTSignOptions: SignOptionsSignature = getSecurity(config).web
+            .sign as SignOptionsSignature;
           res.set(HEADERS.CACHE_CONTROL, 'no-cache, no-store');
           next({
             token: await auth.jwtEncrypt(user, jWTSignOptions),
