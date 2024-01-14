eb6891c585f3020cb2a00e3780f9540459b208d8
Juan Picado <juanpicado19@gmail.com>
2024-01-14 20:10:57 +0100

    remove snapshots

--- packages/ui-components/src/components/Help/Help.test.tsx
+++ packages/ui-components/src/components/Help/Help.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import { render } from '../../test/test-react-testing-library';
+import { render, screen } from '../../test/test-react-testing-library';
 import Help from './Help';
 
 describe('<Help /> component', () => {
   test('should load the component in default state', () => {
-    const { container } = render(<Help />);
-    expect(container.firstChild).toMatchSnapshot();
+    render(<Help />);
+    expect(screen.getByText('help.title')).toBeInTheDocument();
+    expect(screen.getByText('help.sub-title')).toBeInTheDocument();
+    expect(screen.getByText('help.first-step')).toBeInTheDocument();
+    expect(screen.getByText('help.first-step-command-line')).toBeInTheDocument();
+    expect(screen.getByText('help.second-step')).toBeInTheDocument();
+    expect(screen.getByText('help.second-step-command-line')).toBeInTheDocument();
+    expect(screen.getByText('help.third-step')).toBeInTheDocument();
+    expect(screen.getByText('button.learn-more')).toBeInTheDocument();
   });
 });
