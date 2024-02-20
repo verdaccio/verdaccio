ac3277d9709848deb676d47714ce6f5a107e6dd7
Juan Picado <juanpicado19@gmail.com>
2024-02-20 21:29:35 +0100

    fix: ui dialog break pages on open due remark error

---
+++ .changeset/thirty-toes-swim.md
@@ -0,0 +1,6 @@
+---
+'@verdaccio/ui-theme': patch
+'@verdaccio/ui-components': patch
+---
+
+fix: ui dialog break pages on open due remark error
--- e2e/ui/cypress/e2e/home.cy.ts
+++ e2e/ui/cypress/e2e/home.cy.ts
@@ -32,4 +32,49 @@ describe('home spec', () => {
     cy.visit(`${ctx.url}/-/web/detail/@verdaccio/not-found`);
     cy.getByTestId('404').contains(`Sorry, we couldn't find it.`);
   });
+
+  it('should open dialog settings tabs are present', () => {
+    cy.visit(ctx.url);
+    cy.getByTestId('header--tooltip-settings').click();
+
+    cy.contains('Package Managers');
+    cy.contains('Translations');
+  });
+
+  it('should close dialog settings tabs are present', () => {
+    cy.visit(ctx.url);
+    cy.getByTestId('header--tooltip-settings').click();
+    cy.get('#registryInfo--dialog-close').click();
+    // check for content at the dialog should not be there
+    cy.get('#panel1a-header').should('not.exist');
+  });
+
+  it('should expand npm dialog registry details', () => {
+    cy.visit(ctx.url);
+    cy.getByTestId('header--tooltip-settings').click();
+    cy.get('#panel1a-header').click();
+    cy.contains(/npm set registry/);
+    cy.contains(/npm adduser --registry/);
+    cy.contains(/npm profile set password/);
+  });
+
+  it('should expand pnpm dialog registry details', () => {
+    cy.visit(ctx.url);
+    cy.getByTestId('header--tooltip-settings').click();
+    cy.get('#panel3a-header').click();
+    cy.contains(/pnpm set registry/);
+    cy.contains(/pnpm adduser --registry/);
+    cy.contains(/pnpm profile set password/);
+  });
+
+  it('should expand yarn dialog registry details', () => {
+    cy.visit(ctx.url);
+    cy.getByTestId('header--tooltip-settings').click();
+    cy.get('#panel2a-header').click();
+    // some initial explanation
+    cy.contains(/Yarn classic configuration differs from Yarn/);
+    // smoke test matches, (this is deelpy tested in the unit test)
+    cy.contains(/.yarnrc.yml/);
+    cy.contains(/npmRegistryServer:/);
+  });
 });
