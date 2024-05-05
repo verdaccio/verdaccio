bd8703e8717e50abec85a01fec48a5e6c2e85727
Juan Picado <juanpicado19@gmail.com>
2024-05-05 16:53:28 +0200

    feat: add migrateToSecureLegacySignature property (#4621)
    * feat: add migrateToSecureLegacySignature property
    
    * Update config.ts
    
    * changeset
    
    * Update ci.yml
    
    * Update config.spec.ts

--- packages/config/src/token.ts
+++ packages/config/src/token.ts
@@ -1,9 +1,11 @@
 import { randomBytes } from 'crypto';
 
+// TODO: code duplicated at @verdaccio/signature
 export const TOKEN_VALID_LENGTH = 32;
 
 /**
  * Secret key must have 32 characters.
+ * // TODO: code duplicated at @verdaccio/signature
  */
 export function generateRandomSecretKey(): string {
   return randomBytes(TOKEN_VALID_LENGTH).toString('base64').substring(0, TOKEN_VALID_LENGTH);
