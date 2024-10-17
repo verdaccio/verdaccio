bdb7d1fd78e6af8c8790d53656465da345293b4d
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-10-17 17:15:19 -0400

    fix(middleware): error 404 when getting scoped tarballs

--- packages/middleware/test/encode.spec.ts
+++ packages/middleware/test/encode.spec.ts
@@ -1,16 +1,23 @@
+/* eslint-disable no-console */
 import request from 'supertest';
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { HTTP_STATUS } from '@verdaccio/core';
 
 import { encodeScopePackage } from '../src';
 import { getApp } from './helper';
 
-test('encode is json', async () => {
+const testHosts = [
+  'localhost:4873', // with port
+  'myregistry.com', // no port
+  '42.42.42.42', // ip
+  '[2001:db8:85a3:8d3:1319:8a2e:370:7348]:443', // ip6
+];
+
+test('encode is json with relative path', async () => {
   const app = getApp([]);
   // @ts-ignore
   app.use(encodeScopePackage);
-  // @ts-ignore
   app.get('/:id', (req, res) => {
     const { id } = req.params;
     res.status(HTTP_STATUS.OK).json({ id });
@@ -21,84 +28,156 @@ test('encode is json', async () => {
   expect(res.status).toEqual(HTTP_STATUS.OK);
 });
 
-test('packages with version/scope', async () => {
+describe('packages requests', () => {
   const app = getApp([]);
   // @ts-ignore
   app.use(encodeScopePackage);
-  // @ts-ignore
   app.get('/:package/:version?', (req, res) => {
     const { package: pkg, version } = req.params;
     res.status(HTTP_STATUS.OK).json({ package: pkg, version });
   });
 
-  const res = await request(app).get('/foo');
-  expect(res.body).toEqual({ package: 'foo' });
-  expect(res.status).toEqual(HTTP_STATUS.OK);
+  test('just package', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/foo').set('host', host);
+      expect(res.body).toEqual({ package: 'foo' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res2 = await request(app).get('/foo/1.0.0');
-  expect(res2.body).toEqual({ package: 'foo', version: '1.0.0' });
-  expect(res2.status).toEqual(HTTP_STATUS.OK);
+  test('package with version', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/foo/1.0.0').set('host', host);
+      expect(res.body).toEqual({ package: 'foo', version: '1.0.0' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res3 = await request(app).get('/@scope/foo');
-  expect(res3.body).toEqual({ package: '@scope/foo' });
-  expect(res3.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope/foo').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res4 = await request(app).get('/@scope/foo/1.0.0');
-  expect(res4.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
-  expect(res4.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with version', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope/foo/1.0.0').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res5 = await request(app).get('/@scope%2ffoo');
-  expect(res5.body).toEqual({ package: '@scope/foo' });
-  expect(res5.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded path', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope%2ffoo').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res6 = await request(app).get('/@scope%2ffoo/1.0.0');
-  expect(res6.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
-  expect(res6.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package and version with encoded path', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope%2ffoo/1.0.0').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res7 = await request(app).get('/%40scope%2ffoo');
-  expect(res7.body).toEqual({ package: '@scope/foo' });
-  expect(res7.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded @ and path ', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope%2ffoo').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res8 = await request(app).get('/%40scope%2ffoo/1.0.0');
-  expect(res8.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
-  expect(res8.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package and version with encoded @ and path', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope%2ffoo/1.0.0').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res9 = await request(app).get('/%40scope/foo');
-  expect(res9.body).toEqual({ package: '@scope/foo' });
-  expect(res9.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded @', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope/foo').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res10 = await request(app).get('/%40scope/foo/1.0.0');
-  expect(res10.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
-  expect(res10.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package and version with encoded @', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope/foo/1.0.0').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', version: '1.0.0' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 });
 
-test('tarballs with and without scope', async () => {
+describe('tarball requests', () => {
   const app = getApp([]);
   // @ts-ignore
   app.use(encodeScopePackage);
-  // @ts-ignore
   app.get('/:package/-/:filename', (req, res) => {
     const { package: pkg, filename } = req.params;
     res.status(HTTP_STATUS.OK).json({ package: pkg, filename });
   });
 
-  const res = await request(app).get('/foo/-/foo-1.2.3.tgz');
-  expect(res.body).toEqual({ package: 'foo', filename: 'foo-1.2.3.tgz' });
-  expect(res.status).toEqual(HTTP_STATUS.OK);
+  test('just package', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/foo/-/foo-1.2.3.tgz').set('host', host);
+      expect(res.body).toEqual({ package: 'foo', filename: 'foo-1.2.3.tgz' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res2 = await request(app).get('/@scope/foo/-/foo-1.2.3.tgz');
-  expect(res2.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
-  expect(res2.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope/foo/-/foo-1.2.3.tgz').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res3 = await request(app).get('/@scope%2ffoo/-/foo-1.2.3.tgz');
-  expect(res3.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
-  expect(res3.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded path', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/@scope%2ffoo/-/foo-1.2.3.tgz').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res4 = await request(app).get('/%40scope%2ffoo/-/foo-1.2.3.tgz');
-  expect(res4.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
-  expect(res4.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded @ and path', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope%2ffoo/-/foo-1.2.3.tgz').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
 
-  const res5 = await request(app).get('/%40scope/foo/-/foo-1.2.3.tgz');
-  expect(res5.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
-  expect(res5.status).toEqual(HTTP_STATUS.OK);
+  test('scoped package with encoded @', () => {
+    testHosts.forEach(async (host) => {
+      const res = await request(app).get('/%40scope/foo/-/foo-1.2.3.tgz').set('host', host);
+      expect(res.body).toEqual({ package: '@scope/foo', filename: 'foo-1.2.3.tgz' });
+      expect(res.status).toEqual(HTTP_STATUS.OK);
+    });
+  });
+});
+
+test('invalid url', async () => {
+  const app = getApp([]);
+  // @ts-ignore
+  app.use(encodeScopePackage);
+  app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(HTTP_STATUS.OK).json({ id });
+  });
+
+  const res = await request(app).get('/foo').set('host', 'invalid::host');
+  expect(res.status).toEqual(HTTP_STATUS.BAD_REQUEST);
+  expect(res.text).toContain('Invalid URL');
 });
