fe30826492d06593ccc7c1f9a36ba242c879cea9
Juan Picado <juanpicado19@gmail.com>
2024-10-20 14:14:56 +0200

    migrate cli

--- packages/cli/test/utils.spec.ts
+++ packages/cli/test/utils.spec.ts
@@ -1,16 +1,20 @@
+import { describe, expect, test } from 'vitest';
+
 import { isVersionValid } from '../src/utils';
 
-test('valid version node.js', () => {
-  expect(isVersionValid('v14.0.0')).toBeTruthy();
-  expect(isVersionValid('v15.0.0')).toBeTruthy();
-  expect(isVersionValid('v16.0.0')).toBeTruthy();
-  expect(isVersionValid('v17.0.0')).toBeTruthy();
-});
+describe('utils', () => {
+  test('valid version node.js', () => {
+    expect(isVersionValid('v14.0.0')).toBeTruthy();
+    expect(isVersionValid('v15.0.0')).toBeTruthy();
+    expect(isVersionValid('v16.0.0')).toBeTruthy();
+    expect(isVersionValid('v17.0.0')).toBeTruthy();
+  });
 
-test('is invalid version node.js', () => {
-  expect(isVersionValid('v13.0.0')).toBeFalsy();
-  expect(isVersionValid('v12.0.0')).toBeFalsy();
-  expect(isVersionValid('v8.0.0')).toBeFalsy();
-  expect(isVersionValid('v4.0.0')).toBeFalsy();
-  expect(isVersionValid('v0.0.10')).toBeFalsy();
+  test('is invalid version node.js', () => {
+    expect(isVersionValid('v13.0.0')).toBeFalsy();
+    expect(isVersionValid('v12.0.0')).toBeFalsy();
+    expect(isVersionValid('v8.0.0')).toBeFalsy();
+    expect(isVersionValid('v4.0.0')).toBeFalsy();
+    expect(isVersionValid('v0.0.10')).toBeFalsy();
+  });
 });
