6464e6905650ab045a348020e37c0e6a07109533
Marc Bernard <marc@marcbernardtools.com>
2025-03-06 22:06:42 +0100

    chore: middleware package update

--- packages/web/src/api/index.ts
+++ packages/web/src/api/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { rateLimit } from '@verdaccio/middleware';
+import { WebUrlsNamespace, rateLimit } from '@verdaccio/middleware';
 
 import { hasLogin } from '../web-utils';
 import packageApi from './package';
@@ -12,19 +12,19 @@ import user from './user';
 export default (auth, storage, config) => {
   const route = Router(); /* eslint new-cap: 0 */
   route.use(
-    '/data/',
+    WebUrlsNamespace.data,
     rateLimit({
       windowMs: 2 * 60 * 1000, // 2  minutes
       max: 5000, // limit each IP to 1000 requests per windowMs
       ...config?.web?.rateLimit,
     })
   );
-  route.use('/data/', packageApi(storage, auth, config));
-  route.use('/data/', search(storage, auth));
-  route.use('/data/', sidebar(config, storage, auth));
-  route.use('/data/', readme(storage, auth));
+  route.use(WebUrlsNamespace.data, packageApi(storage, auth, config));
+  route.use(WebUrlsNamespace.data, search(storage, auth));
+  route.use(WebUrlsNamespace.data, sidebar(config, storage, auth));
+  route.use(WebUrlsNamespace.data, readme(storage, auth));
   if (hasLogin(config)) {
-    route.use('/sec/', user(auth, config));
+    route.use(WebUrlsNamespace.sec, user(auth, config));
   }
   return route;
 };
