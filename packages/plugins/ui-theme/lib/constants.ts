c81bd756160df061f53d018c0412a6cd076efda2
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2025-03-17 08:02:26 +0100

    chore(ui): update react-json-view (#5144)
    * chore(ui): update react-json-view
    
    * update lockfile

--- packages/plugins/ui-theme/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-export const DIST_TAGS = 'dist-tags';
-
-export const HEADERS = {
-  JSON: 'application/json',
-};
-
-export const API_ERROR = {
-  BAD_USERNAME_PASSWORD: 'bad username/password, access denied',
-};
