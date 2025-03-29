baf3c9ac1676c17326e1a31f6576427cce9e4a17
Juan Picado <juanpicado19@gmail.com>
2025-03-29 18:03:11 +0100

    fix: add legacy getMatchedPackagesSpec to config
    Some old plugins depends of this function

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,13 @@
-import _ from 'lodash';
-
 import { Config as ConfigCore } from '@verdaccio/config';
+import { getMatchedPackagesSpec } from '@verdaccio/utils';
 
 class Config extends ConfigCore {
   public constructor(config: any) {
     config.configPath = config.self_path;
     super(config, { forceMigrateToSecureLegacySignature: false });
+    // Temporary solution for plugins that depepends of legacy configuration files
+    // @ts-ignore
+    this.getMatchedPackagesSpec = getMatchedPackagesSpec;
   }
 }
 
