d33266b5a65b146544115dc27b00d64ecdaba109
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2025-05-17 16:11:01 +0200

    chore(types): use "Person" instead of "Author" (#5232)

--- packages/store/src/storage.ts
+++ packages/store/src/storage.ts
@@ -2072,7 +2072,13 @@ class Storage {
       this.config?.publish?.check_owners === true &&
       manifest.maintainers &&
       manifest.maintainers.length > 0 &&
-      !manifest.maintainers.some((maintainer) => maintainer.name === username)
+      !manifest.maintainers.some((maintainer) => {
+        if (typeof maintainer === 'string') {
+          return maintainer === username;
+        } else {
+          return maintainer.name === username;
+        }
+      })
     ) {
       this.logger.error({ username }, '@{username} is not a maintainer (package owner)');
       throw Error('only owners are allowed to change package');
