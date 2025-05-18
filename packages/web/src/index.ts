cc8269a4d3925f7c8b3f15925210ff3602188780
Marc Bernard <marc@marcbernardtools.com>
2025-05-18 09:09:32 +0200

    fix plugin_prefix

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,3 +1,3 @@
-export { default, PLUGIN_UI_PREFIX, DEFAULT_PLUGIN_UI_THEME } from './middleware';
+export { default, DEFAULT_PLUGIN_UI_THEME } from './middleware';
 export * from './author-utils';
 export * from './web-utils';
