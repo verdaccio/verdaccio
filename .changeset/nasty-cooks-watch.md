a0860a0d17a674b43f7672e64df5317d8564ba7b
Marc Bernard <marc@marcbernardtools.com>
2025-05-21 21:35:17 +0200

    Fix publish test

--- .changeset/nasty-cooks-watch.md
+++ .changeset/nasty-cooks-watch.md
@@ -1,5 +1,6 @@
 ---
 '@verdaccio/e2e-cli-npm11': patch
+'@verdaccio/e2e-cli-npm-common': patch
 ---
 
 feat(e2e): add npm11
--- e2e/cli/e2e-npm-commons/publish.ts
+++ e2e/cli/e2e-npm-commons/publish.ts
@@ -15,9 +15,15 @@ export function runPublish(npm) {
     test.each([['verdaccio-memory', 'verdaccio', '@verdaccio/foo', '@verdaccio/some-foo']])(
       'should publish a package %s',
       async (pkgName) => {
+        // As of npm v11, npm will fetch the packument from the npm registry before publishing (from the uplink),
+        // and there are more checks in the client:
+        // - "You cannot publish over the previously published versions"
+        // - "You must specify a tag using --tag when publishing a prerelease version"
+        // - "Cannot implicitly apply the 'latest' tag because previously published version x.y.z is higher than the new version a.b.c"
+        // Therefore, we pick a version that is higher than any of the published versions for the test packages (and not a pre-release).
         const { tempFolder } = await prepareGenericEmptyProject(
           pkgName,
-          '1.0.0',
+          '99.0.0',
           registry.port,
           registry.getToken(),
           registry.getRegistryUrl()
