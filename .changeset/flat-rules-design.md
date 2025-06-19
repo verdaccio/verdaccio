5cf592090feaede98842c8cbde696cba839325c3
Marc Bernard <marc@marcbernardtools.com>
2025-06-19 10:07:46 +0200

    chore(deps): verdaccio package

---
+++ .changeset/flat-rules-design.md
@@ -0,0 +1,5 @@
+---
+'verdaccio': patch
+---
+
+chore(deps): verdaccio package
--- packages/verdaccio/test/disabled_test/__helper/index.ts
+++ packages/verdaccio/test/disabled_test/__helper/index.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 
 export const parseConfigurationFile = (name) => {
   return path.join(__dirname, `../partials/config/yaml/${name}.yaml`);
