1812e907cb652e18ab51d8769a932d7cd22e9af8
Juan Picado <juanpicado19@gmail.com>
2025-07-27 14:11:13 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 export { startVerdaccio as default, startVerdaccio } from './lib/bootstrap';
 export { runServer } from './lib/run-server';
 export { ConfigBuilder, parseConfigFile, getDefaultConfig, Config } from '@verdaccio/config';
-export {
-  fileUtils,
-  errorUtils,
-  cryptoUtils,
-  warningUtils,
-  pkgUtils,
-  authUtils,
-} from '@verdaccio/core';
+export { fileUtils, errorUtils, cryptoUtils, pkgUtils } from '@verdaccio/core';
