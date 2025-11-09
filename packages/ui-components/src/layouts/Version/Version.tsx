a85be6f2380029fcf72e0c7054a62389a6f7e033
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2025-11-09 17:23:43 -0500

    fix(ui): regression after MUI 7 upgrade

--- packages/ui-components/src/layouts/Version/Version.tsx
+++ packages/ui-components/src/layouts/Version/Version.tsx
@@ -6,10 +6,10 @@ import { Detail, SideBar } from '../../sections';
 const VersionLayout: React.FC = () => {
   return (
     <Grid className={'container content'} container={true} data-testid="version-layout" spacing={0}>
-      <Grid item={true} md={8} xs={12}>
+      <Grid size={{ md: 8, xs: 12 }}>
         <Detail />
       </Grid>
-      <Grid item={true} md={4} xs={12}>
+      <Grid size={{ md: 4, xs: 12 }}>
         <SideBar />
       </Grid>
     </Grid>
