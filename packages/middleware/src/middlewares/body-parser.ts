900fa2579c4a9826e086400468b9be10eefb0033
Marc Bernard <marc@marcbernardtools.com>
2026-01-15 23:29:19 +0100

    prevent prototype pollution

--- packages/middleware/src/middlewares/body-parser.ts
+++ packages/middleware/src/middlewares/body-parser.ts
@@ -25,6 +25,19 @@ export function registerBodyParser(app: Router, config: Config): void {
   if (hasBodyParser(app)) {
     debug('json parser already registered');
   } else {
-    app.use(express.json({ strict: false, limit: config.max_body_size || '10mb' }));
+    app.use(
+      express.json({
+        strict: false,
+        limit: config.max_body_size || '10mb',
+        // Prevent prototype pollution (explicitly reject __proto__ and constructor)
+        // https://medium.com/@instatunnel/prototype-pollution-the-javascript-vulnerability-that-poisons-your-entire-app-%EF%B8%8F-bad082616771
+        verify: (_req, _res, buf) => {
+          const str = buf.toString();
+          if (str.includes('__proto__') || str.includes('constructor')) {
+            throw new Error('Invalid JSON');
+          }
+        },
+      })
+    );
   }
 }
