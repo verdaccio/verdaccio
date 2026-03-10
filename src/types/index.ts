16214d8109b47c617f97832858aa2497512eed42
Juan Picado <juanpicado19@gmail.com>
2026-03-10 19:33:33 +0100

    fix express build

--- src/types/index.ts
+++ src/types/index.ts
@@ -60,7 +60,7 @@ export interface Profile {
   fullname: string;
 }
 
-export type $RequestExtend = Request & { remote_user?: any; log: Logger };
+export type $RequestExtend = Omit<Request, 'params'> & { remote_user?: any; log: Logger; params: Record<string, string> };
 export type $ResponseExtend = Response & { cookies?: any };
 export type $NextFunctionVer = NextFunction & any;
 export type $SidebarPackage = Manifest & { latest: any };
