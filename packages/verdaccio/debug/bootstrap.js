e8a2f44dea54db71502e70df37ca80f73f0c524e
Juan Picado <juanpicado19@gmail.com>
2026-03-18 19:23:38 +0100

    fix pnpm start

--- packages/verdaccio/debug/bootstrap.js
+++ packages/verdaccio/debug/bootstrap.js
@@ -1,5 +1,4 @@
 // this file aims to help local debugging with hot transpilation
-require('@babel/register')({
-  extensions: ['.ts'],
-});
+// uses tsx for fast TypeScript execution (replaces @babel/register)
+require('tsx/cjs');
 require('@verdaccio/cli');
