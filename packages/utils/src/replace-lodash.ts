359844e0c180c1b9acb84bfd9d76f17c20d448d6
Marc Bernard <marc@marcbernardtools.com>
2026-03-26 08:15:47 +0100

    chore: remove unused @verdaccio/utils

--- packages/utils/src/replace-lodash.ts
+++
@@ -1,7 +0,0 @@
-export function isNil(value: any): boolean {
-  return value === null || typeof value === 'undefined';
-}
-
-export function isFunction(value): boolean {
-  return typeof value === 'function';
-}
