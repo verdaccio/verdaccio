372405432682045b5e97c0eb778c018075c6eabc
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2026-09-09 16:36:06 -0400

    chore(loader): remove console log (#6237)

--- packages/loaders/test/partials/test-plugin-storage/verdaccio-plugin/index.js
+++ packages/loaders/test/partials/test-plugin-storage/verdaccio-plugin/index.js
@@ -2,7 +2,6 @@ class ValidVerdaccioPlugin {
   config;
   options;
   constructor(config, options) {
-    console.log('ValidVerdaccioPlugin constructor', config);
     this.config = config;
     this.options = options;
   }
